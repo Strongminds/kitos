d2c49077e3b6659ec0e3a1450ac340263fb1a7a9
JacobMalling <jacob.malling.olesen@gmail.com>
2019-07-09 09:46:54 +0200

    Fixed test running script review

--- DeploymentScripts/RunE2ETests.ps1
+++ DeploymentScripts/RunE2ETests.ps1
@@ -7,37 +7,54 @@ param(
 
 Try 
 {
-    $oldSeleniumServer = Get-Process -Id (Get-NetTCPConnection -LocalPort 4445 -ErrorAction Stop).OwningProcess | Where-Object {$_.ProcessName.Equals("java")}
+    $oldSeleniumServer = Get-Process -Id (Get-NetTCPConnection -LocalPort 4444 -ErrorAction Stop).OwningProcess | Where-Object {$_.ProcessName.Equals("java")}
 
     if ($oldSeleniumServer){
+        Write-Host "Found unexpected java process on port 4444. Trying to shut down"
         kill -Id $oldSeleniumServer.Id
+		$maxWait = 0
         while(-Not $oldSeleniumServer.HasExited){
-            Write-Output "Not yet killed"
+            Write-Host "Shutting down..."
             sleep 1
+			$maxWait += 1
+			if($maxWait -ge 10){
+				throw "Taking too long to close selenium server on port 4444. Please try again or shutdown manually."
+			}
         }
+        Write-Host "Succesfully shutdown"
     }
 }
 Catch [Microsoft.PowerShell.Cmdletization.Cim.CimJobException]
 {
-
+    Write-Host "Found nothing using port 4444 as expected. Moving on to starting selenium server"
 }
 Catch
 {
-    Write-Host "An unexpected error ocurred. Is your selenium server shut-down correctly?"
+    throw "An unexpected error ocurred. Is your selenium server shut-down correctly?"
 }
 
 try {
+    Write-Host "Starting selenium server"
     $app = Start-Process powershell.exe -ArgumentList "webdriver-manager start" -PassThru -WindowStyle Hidden
-    $gulpApp = Start-Process powershell.exe -ArgumentList "gulp e2e:headless --params.login.email='$usrname' --params.login.pwd='$pwd' --baseUrl='$url'" -PassThru -Wait -WindowStyle Hidden
+
+    Write-Host "Starting E2E test. This might take a while..."
+    $gulpApp = Start-Process powershell.exe -ArgumentList "gulp e2e:headless --params.login.email='$usrname' --params.login.pwd='$pwd' --baseUrl='$url'" -PassThru -Wait -WindowStyle Hidden -RedirectStandardOutput testOutput.txt
     
+    If($gulpApp.ExitCode.Equals(0)){
+        Write-Host "All tests succeeded :D"
+    }
     If ($gulpApp.ExitCode.Equals(1)){
-        throw "At least 1 E2E test failed, check tmp folder for log and report"
+        Get-Content -Path testOutput.txt
+        throw "At least 1 E2E test failed, check tmp folder for detailed report"
     }
 }
 catch{
-    Write-Host "An unexpected error ocurred. Not sure what could have caused this. Please try again or investigate..."
+    throw $_.Exception
 }
 finally{
+    Remove-Item -Path testOutput.txt
+
+    Write-Host "Testing done, shutting down selenium server as clean up"
     Stop-Process $app.Id
 
     $seleniumServer = Get-Process -Id (Get-NetTCPConnection -LocalPort 4444).OwningProcess | Where-Object {$_.ProcessName.Equals("java")}
