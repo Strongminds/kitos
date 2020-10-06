adb64d3874a2d72158441747d7fb68d85537b590
Mads Brønd Kracht <mbk@strongminds.dk>
2020-10-06 15:20:17 +0200

    Updated migration

--- Infrastructure.DataAccess/Migrations/202010061318213_Added_Agreement_Oversight_Completed_Date_And_Remark.Designer.cs
+++ Infrastructure.DataAccess/Migrations/202010061318213_Added_Agreement_Oversight_Completed_Date_And_Remark.Designer.cs
@@ -13,7 +13,7 @@ namespace Infrastructure.DataAccess.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "202010021224199_Added_Agreement_Oversight_Completed_Date_And_Remark"; }
+            get { return "202010061318213_Added_Agreement_Oversight_Completed_Date_And_Remark"; }
         }
         
         string IMigrationMetadata.Source
