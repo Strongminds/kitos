f8a5ec12cba5def077768e23d11c0e90873f348b
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2021-04-12 14:53:39 +0200

    fix

--- Infrastructure.DataAccess/Migrations/202104071049413_Contract_Remove_DataHandler.cs
+++ Infrastructure.DataAccess/Migrations/202104071049413_Contract_Remove_DataHandler.cs
@@ -1,17 +1,28 @@
-﻿namespace Infrastructure.DataAccess.Migrations
+﻿using System.Runtime.Remoting.Lifetime;
+
+namespace Infrastructure.DataAccess.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class Contract_Remove_DataHandler : DbMigration
     {
         public override void Up()
         {
-            DropForeignKey("dbo.ItContract", "DataHandlerId", "dbo.ItContract");
-            DropIndex("dbo.ItContract", new[] { "DataHandlerId" });
-            DropColumn("dbo.ItContract", "DataHandlerId");
+            var leaseTime = LifetimeServices.LeaseTime;
+            try
+            {
+                LifetimeServices.LeaseTime = TimeSpan.FromHours(1);
+                DropForeignKey("dbo.ItContract", "DataHandlerId", "dbo.ItContract");
+                DropIndex("dbo.ItContract", new[] { "DataHandlerId" });
+                DropColumn("dbo.ItContract", "DataHandlerId");
+            }
+            finally
+            {
+                LifetimeServices.LeaseTime = leaseTime;
+            }
         }
-        
+
         public override void Down()
         {
             AddColumn("dbo.ItContract", "DataHandlerId", c => c.Int());
