df6c53aa812b05592f6b723adba82eb5e86606d0
Morten Rembøl Jacobsen <mrj@strongminds.dk>
2023-01-10 08:24:46 +0100

    added proof implementation as inspiration for the hotfix - Detect the primary org unit based on selection in the Organization - Use the info to filter the org units

---
+++ Tests.Unit.Presentation.Web/Authorization/StsOrgTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Core.Abstractions.Types;
+using Core.DomainModel.Organization;
+using Core.DomainServices.Repositories.Organization;
+using Core.DomainServices.SSO;
+using Infrastructure.STS.Company.DomainServices;
+using Infrastructure.STS.Organization.DomainServices;
+using Infrastructure.STS.OrganizationUnit.DomainServices;
+using Moq;
+using Serilog;
+using Xunit;
+
+namespace Tests.Unit.Presentation.Web.Authorization
+{
+    public class StsOrgTest
+    {
+        class repoStuvb : IStsOrganizationIdentityRepository
+        {
+            public Maybe<StsOrganizationIdentity> GetByExternalUuid(Guid externalId)
+            {
+                return Maybe<StsOrganizationIdentity>.None;
+            }
+
+            public Result<StsOrganizationIdentity, OperationError> AddNew(Organization organization, Guid externalId)
+            {
+                return new StsOrganizationIdentity(externalId, organization);
+            }
+        }
+
+        [Fact]
+        public void TestMiddelfart()
+        {
+            var configuration = new StsOrganisationIntegrationConfiguration("44d05370a7bcfb9fa9958341e2171e89de26d6a1", "prod.serviceplatformen.dk");
+            var stsOrgService = new StsOrganizationService(configuration,
+                new StsOrganizationCompanyLookupService(configuration, Mock.Of<ILogger>()),
+                new repoStuvb(), Mock.Of<ILogger>());
+
+            var service = new StsOrganizationUnitService(stsOrgService, configuration, Mock.Of<ILogger>());
+
+
+            var result = service.ResolveOrganizationTree(new Organization() { Cvr = "29189684" });
+        }
+    }
+}
