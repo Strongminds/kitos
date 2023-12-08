c742812b630598b012d2e27c0d6f97980d06e50a
Aleksander Naskret <aln@strongminds.dk>
2023-12-08 12:55:43 +0100

    cleanup

--- Presentation.Web/LoginHandler.ashx.cs
+++ Presentation.Web/LoginHandler.ashx.cs
@@ -3,14 +3,14 @@
 namespace Presentation.Web
 {
     /// <summary>
-    /// Summary description for MyHandler
+    /// Summary description for LoginHandler
     /// </summary>
-    public class MyHandler : IHttpHandler
+    public class LoginHandler : IHttpHandler
     {
 
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.Redirect("Login.ashx");
+            context.Response.Redirect("Login.ashx?forceAuthn=true");
         }
 
         public bool IsReusable
