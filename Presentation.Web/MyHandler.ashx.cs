40340c96ab00795433b302ba16289e1f636aa603
Aleksander Naskret <aln@strongminds.dk>
2023-12-08 11:48:25 +0100

    test handler

---
+++ Presentation.Web/MyHandler.ashx.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+
+namespace Presentation.Web
+{
+    /// <summary>
+    /// Summary description for MyHandler
+    /// </summary>
+    public class MyHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
