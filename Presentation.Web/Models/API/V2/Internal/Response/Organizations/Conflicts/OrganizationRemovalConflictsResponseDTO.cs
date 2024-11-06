2dcded8568d320b9923df9ad838749621369c884
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-06 13:58:13 +0100

    add new dto files, not final

---
+++ Presentation.Web/Models/API/V2/Internal/Response/Organizations/Conflicts/OrganizationRemovalConflictsResponseDTO.cs
@@ -0,0 +1,24 @@
+﻿using Presentation.Web.Models.API.V1.Organizations;
+using Presentation.Web.Models.API.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Presentation.Web.Models.API.V2.Response.Generic.Identity;
+using Presentation.Web.Models.API.V2.Internal.Response.Organizations.Conflicts;
+
+namespace Presentation.Web.Models.API.V2.Internal.Response.Organizations
+{
+    public class OrganizationRemovalConflictsResponseDTO
+    {
+        public IEnumerable<MultipleConflictsResponseDTO> SystemsWithUsagesOutsideTheOrganization { get; set; }
+        public IEnumerable<MultipleConflictsResponseDTO> InterfacesExposedOnSystemsOutsideTheOrganization { get; set; }
+        public IEnumerable<MultipleConflictsResponseDTO> SystemsExposingInterfacesDefinedInOtherOrganizations { get; set; }
+        public IEnumerable<MultipleConflictsResponseDTO> SystemsSetAsParentSystemToSystemsInOtherOrganizations { get; set; }
+        public IEnumerable<SimpleConflictResponseDTO> DprInOtherOrganizationsWhereOrgIsDataProcessor { get; set; }
+        public IEnumerable<SimpleConflictResponseDTO> DprInOtherOrganizationsWhereOrgIsSubDataProcessor { get; set; }
+        public IEnumerable<SimpleConflictResponseDTO> ContractsInOtherOrganizationsWhereOrgIsSupplier { get; set; }
+        public IEnumerable<SimpleConflictResponseDTO> SystemsInOtherOrganizationsWhereOrgIsRightsHolder { get; set; }
+        public IEnumerable<SimpleConflictResponseDTO> SystemsWhereOrgIsArchiveSupplier { get; set; }
+    }
+}
