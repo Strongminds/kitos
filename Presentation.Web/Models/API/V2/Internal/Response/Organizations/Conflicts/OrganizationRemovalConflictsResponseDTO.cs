eaf5c4feea6968165cb3d71a3e5c7190e1799cb4
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-07 13:56:59 +0100

    remove unused imports

--- Presentation.Web/Models/API/V2/Internal/Response/Organizations/Conflicts/OrganizationRemovalConflictsResponseDTO.cs
+++ Presentation.Web/Models/API/V2/Internal/Response/Organizations/Conflicts/OrganizationRemovalConflictsResponseDTO.cs
@@ -1,11 +1,5 @@
-﻿using Presentation.Web.Models.API.V1.Organizations;
-using Presentation.Web.Models.API.V1;
-using System;
+﻿using Presentation.Web.Models.API.V2.Internal.Response.Organizations.Conflicts;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Presentation.Web.Models.API.V2.Response.Generic.Identity;
-using Presentation.Web.Models.API.V2.Internal.Response.Organizations.Conflicts;
 
 namespace Presentation.Web.Models.API.V2.Internal.Response.Organizations
 {
