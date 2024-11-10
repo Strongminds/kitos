a87c5d72df6983b62d349c77f8470214c3e427d2
NikolajNielsen <nan@strongminds.dk>
2024-11-10 17:28:04 +0100

    Can get helptexts

--- Core.ApplicationServices/HelpTexts/HelpTextService.cs
+++ Core.ApplicationServices/HelpTexts/HelpTextService.cs
@@ -1,16 +1,27 @@
 ﻿
 using System.Collections.Generic;
+using System.Linq;
 using Core.Abstractions.Types;
+using Core.ApplicationServices.Authorization;
 using Core.DomainModel;
+using Core.DomainServices;
 using NotImplementedException = System.NotImplementedException;
 
 namespace Core.ApplicationServices.HelpTexts
 {
     public class HelpTextService: IHelpTextService
     {
+        private readonly IGenericRepository<HelpText> _helpTextsRepository;
+        private readonly IAuthorizationContext _authorizationContext;
+
+        public HelpTextService(IGenericRepository<HelpText> helpTextsRepository)
+        {
+            this._helpTextsRepository = helpTextsRepository;
+        }
+
         public Result<IEnumerable<HelpText>, OperationError> GetHelpTexts()
         {
-            throw new NotImplementedException();
+            return Result<IEnumerable<HelpText>, OperationError>.Success(this._helpTextsRepository.AsQueryable().ToList());
         }
 
         public Result<HelpText, OperationError> CreateHelpText()
--- Tests.Unit.Core.ApplicationServices/ApplicationServices/HelpTexts/HelpTextServiceTest.cs
+++ Tests.Unit.Core.ApplicationServices/ApplicationServices/HelpTexts/HelpTextServiceTest.cs
@@ -1,6 +1,12 @@
 ﻿
 
+using System.Collections.Generic;
+using System.Linq;
+using Core.ApplicationServices.Authorization;
 using Core.ApplicationServices.HelpTexts;
+using Core.DomainModel;
+using Core.DomainServices;
+using Moq;
 using Tests.Toolkit.Patterns;
 using Xunit;
 
@@ -9,17 +15,32 @@ namespace Tests.Unit.Core.ApplicationServices.HelpTexts
     public class HelpTextServiceTest: WithAutoFixture
     {
         private HelpTextService _sut;
+        private Mock<IGenericRepository<HelpText>> _helpTextsRepository;
+        private Mock<IAuthorizationContext> _authorizationContext;
 
         public HelpTextServiceTest()
         {
-            _sut = new HelpTextService();
+            _helpTextsRepository = new Mock<IGenericRepository<HelpText>>();
+            _sut = new HelpTextService(_helpTextsRepository.Object);
         }
 
         [Fact]
         public void Can_Get_Help_Texts()
         {
-            //todo test
+            var expected = new HelpText()
+            {
+                Description = A<string>(),
+                Key = A<string>(),
+                Title = A<string>()
+            };
+            _helpTextsRepository.Setup(_ => _.AsQueryable()).Returns(new List<HelpText>(){expected}.AsQueryable());
+
             var result = _sut.GetHelpTexts();
+
+            Assert.True(result.Ok);
+            var helpText = result.Value.FirstOrDefault();
+            Assert.NotNull(helpText);
+            Assert.Equivalent(expected, helpText);
         }
     }
 }
