ac8713c0709ad8424afd89b312386699a26ad776
NikolajNielsen <nan@strongminds.dk>
2024-11-11 10:26:58 +0100

    Api setup for get

---
+++ Presentation.Web/Controllers/API/V2/Internal/HelpTextsInternalV2Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+using Core.ApplicationServices.HelpTexts;
+using Presentation.Web.Controllers.API.V2.Internal.Mapping;
+using Presentation.Web.Models.API.V2.Internal.Response;
+using Swashbuckle.Swagger.Annotations;
+
+namespace Presentation.Web.Controllers.API.V2.Internal
+{
+    [RoutePrefix("api/v2/internal/help-texts")]
+    public class HelpTextsInternalV2Controller: InternalApiV2Controller
+    {
+        private readonly IHelpTextApplicationService _helpTextApplicationService;
+        private readonly IHelpTextResponseMapper _responseMapper;
+
+        public HelpTextsInternalV2Controller(IHelpTextApplicationService helpTextApplicationService, IHelpTextResponseMapper responseMapper)
+        {
+            _helpTextApplicationService = helpTextApplicationService;
+            _responseMapper = responseMapper;
+        }
+
+        [HttpGet]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(IEnumerable<HelpTextResponseDTO>))]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        public IHttpActionResult Get()
+        {
+            return _helpTextApplicationService.GetHelpTexts()
+                .Select(_responseMapper.ToResponseDTOs)
+                .Match(Ok, FromOperationError);
+        }
+    }
+}
