6c609531f748266d6cc248e2342d9b305f7d5922
NikolajNielsen <nan@strongminds.dk>
2024-11-19 08:21:33 +0100

    Add db mapping for organization

--- Core.DomainModel/Organization/CountryCode.cs
+++ Core.DomainModel/Organization/CountryCode.cs
@@ -5,7 +5,7 @@ namespace Core.DomainModel.Organization
     /// <summary>
     /// Country code for use in the organization countryCode field.
     /// </summary>
-    public class CountryCode: OptionEntity<Organization>, IOptionReference<Organization>, IHasUuid
+    public class CountryCode: OptionEntity<Organization>, IOptionReference<Organization>
     {
         public virtual ICollection<Organization> References { get; set; } = new HashSet<Organization>();
     }
