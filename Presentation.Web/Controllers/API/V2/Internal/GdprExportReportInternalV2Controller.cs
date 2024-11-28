14a251b8167ea54398bc9c5346c8dacf766f9930
JacobBorchMadsen <jbma@strongminds.dk>
2024-11-28 15:23:25 +0100

    refine controller

--- Presentation.Web/Controllers/API/V2/Internal/GdprExportReportInternalV2Controller.cs
+++ Presentation.Web/Controllers/API/V2/Internal/GdprExportReportInternalV2Controller.cs
@@ -1,26 +1,21 @@
 ﻿using Core.Abstractions.Types;
 using Core.ApplicationServices.SystemUsage.GDPR;
-using Core.DomainModel.ItSystem.DataTypes;
 using Core.DomainModel.ItSystemUsage.GDPR;
 using Core.DomainModel.Organization;
 using Core.DomainServices.Generic;
 using Presentation.Web.Infrastructure.Attributes;
 using Presentation.Web.Models.API.V2.Internal.Response;
-using Presentation.Web.Models.API.V2.Types.Shared;
 using Swashbuckle.Swagger.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Http;
-using Presentation.Web.Controllers.API.V2.Common.Mapping;
 using Presentation.Web.Controllers.API.V2.External.ItSystemUsages.Mapping;
-
 
 namespace Presentation.Web.Controllers.API.V2.Internal
 {
-
+     
     [RoutePrefix("api/v2/internal/gdpr-report")]
     public class GdprExportReportInternalV2Controller : InternalApiV2Controller
     {
