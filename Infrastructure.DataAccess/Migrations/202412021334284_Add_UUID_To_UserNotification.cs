38c0dc686299e6cac650569d976ad72b184a82d3
JacobBorchMadsen <jbma@strongminds.dk>
2024-12-13 10:51:40 +0100

    fix migration

--- Infrastructure.DataAccess/Migrations/202412021334284_Add_UUID_To_UserNotification.cs
+++ Infrastructure.DataAccess/Migrations/202412021334284_Add_UUID_To_UserNotification.cs
@@ -1,20 +1,25 @@
 ﻿using System.Data.Entity.Migrations;
-
 namespace Infrastructure.DataAccess.Migrations
 {
     public partial class Add_UUID_To_UserNotification : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.UserNotifications", "Uuid", c => c.Guid(nullable: false));
+            AddColumn("dbo.UserNotifications", "Uuid", c => c.Guid(nullable: true));
+            Sql(@"
+                UPDATE dbo.UserNotifications 
+                SET Uuid = NEWID() 
+                WHERE Uuid IS NULL 
+                   OR Uuid = '00000000-0000-0000-0000-000000000000'
+            ");
+            AlterColumn("dbo.UserNotifications", "Uuid", c => c.Guid(nullable: false));
             CreateIndex("dbo.UserNotifications", "Uuid", unique: true);
         }
 
         public override void Down()
         {
-            DropIndex("dbo.UserNotifications", "Uuid");
+            DropIndex("dbo.UserNotifications", new[] { "Uuid" });
             DropColumn("dbo.UserNotifications", "Uuid");
         }
-
     }
 }
