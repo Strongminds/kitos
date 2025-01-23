2adb17fa685a32d285628be6eaf809adcab17dab
JacobBorchMadsen <jbma@strongminds.dk>
2025-01-23 16:08:14 +0100

    added migration files

---
+++ Infrastructure.DataAccess/Migrations/202501221342374_ChangeSystemUsageExternalUuidToString.cs
@@ -0,0 +1,17 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeSystemUsageExternalUuidToString : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.ItSystemUsageOverviewReadModels", "ExternalSystemUuid", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.ItSystemUsageOverviewReadModels", "ExternalSystemUuid", c => c.Guid());
+        }
+    }
+}
