24c6f8fe8bea20dabaa7cf244e3befccfa9b7e21
Aleksander Naskret <aln@strongminds.dk>
2025-01-24 07:32:04 +0100

    migration

---
+++ Infrastructure.DataAccess/Migrations/202501240629208_UpdateParentItSystemUsageUuidValues.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using Infrastructure.DataAccess.Tools;
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateParentItSystemUsageUuidValues : DbMigration
+    {
+        public override void Up()
+        {
+            SqlResource(SqlMigrationScriptRepository.GetResourceName("Patch_ParentItSystemUsageUuid.sql"));
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
