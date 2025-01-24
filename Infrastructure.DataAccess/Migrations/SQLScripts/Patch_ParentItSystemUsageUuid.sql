c75cf9c24903ad0bf308ebda976dab3dafc350b0
Aleksander Naskret <aln@strongminds.dk>
2025-01-24 07:26:51 +0100

    sql query

---
+++ Infrastructure.DataAccess/Migrations/SQLScripts/Patch_ParentItSystemUsageUuid.sql
@@ -0,0 +1,12 @@
+﻿BEGIN
+    UPDATE [ItSystemUsageOverviewReadModels]
+    SET ParentItSystemUsageUuid = 
+    (
+        SELECT t3.Uuid FROM ItSystemUsage t1
+        INNER JOIN ItSystem t2 
+        ON t1.ItSystemId = t2.Id
+        LEFT JOIN ItSystem t3
+        ON t2.ParentId = t3.Id
+        WHERE t1.Uuid = [ItSystemUsageOverviewReadModels].SourceEntityUuid
+    );
+END
