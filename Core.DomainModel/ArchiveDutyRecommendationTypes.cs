cb5c1fd05384b14e2f2828877455d194b983720d
JacobBorchMadsen <jbma@strongminds.dk>
2025-01-30 08:54:02 +0100

    extend api model for archive duty

--- Core.DomainModel/ArchiveDutyRecommendationTypes.cs
+++ Core.DomainModel/ArchiveDutyRecommendationTypes.cs
@@ -19,7 +19,7 @@
         /// </summary>
         NoRecommendation = 3,
         /// <summary>
-        /// Recommends data is kept, but selected or all documents are discarded
+        /// Recommends data is kept, selected or all documents are discarded
         /// </summary>
         PreserveDataDiscardOption = 4,
     }
