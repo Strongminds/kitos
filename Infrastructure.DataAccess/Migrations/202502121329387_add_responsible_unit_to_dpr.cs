3a264c86eeaac16fc11d458f44487db4e873e691
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-12 14:31:36 +0100

    add migration files

---
+++ Infrastructure.DataAccess/Migrations/202502121329387_add_responsible_unit_to_dpr.cs
@@ -0,0 +1,27 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_responsible_unit_to_dpr : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.DataProcessingRegistrations", "ResponsibleOrganizationUnitId", c => c.Int());
+            AddColumn("dbo.DataProcessingRegistrationReadModels", "ResponsibleOrgUnitId", c => c.Int());
+            AddColumn("dbo.DataProcessingRegistrationReadModels", "ResponsibleOrgUnitName", c => c.String());
+            CreateIndex("dbo.DataProcessingRegistrations", "ResponsibleOrganizationUnitId");
+            CreateIndex("dbo.DataProcessingRegistrationReadModels", "ResponsibleOrgUnitId", name: "IX_DPR_ResponsibleOrgUnitId");
+            AddForeignKey("dbo.DataProcessingRegistrations", "ResponsibleOrganizationUnitId", "dbo.OrganizationUnit", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.DataProcessingRegistrations", "ResponsibleOrganizationUnitId", "dbo.OrganizationUnit");
+            DropIndex("dbo.DataProcessingRegistrationReadModels", "IX_DPR_ResponsibleOrgUnitId");
+            DropIndex("dbo.DataProcessingRegistrations", new[] { "ResponsibleOrganizationUnitId" });
+            DropColumn("dbo.DataProcessingRegistrationReadModels", "ResponsibleOrgUnitName");
+            DropColumn("dbo.DataProcessingRegistrationReadModels", "ResponsibleOrgUnitId");
+            DropColumn("dbo.DataProcessingRegistrations", "ResponsibleOrganizationUnitId");
+        }
+    }
+}
