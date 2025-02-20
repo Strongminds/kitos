3135dc01a7856682b51258301af3a975e012e3c2
NikolajNielsen <nan@strongminds.dk>
2025-02-20 20:12:04 +0100

    Add test project

---
+++ PubSub.Test/Unit/RabbitMQPublisherTest.cs
@@ -0,0 +1,11 @@
+namespace PubSub.Test.Unit
+{
+    public class RabbitMQPublisherTest
+    {
+        [Fact]
+        public void Can_Publish_Serialized_Message_On_Channel()
+        {
+
+        }
+    }
+}
