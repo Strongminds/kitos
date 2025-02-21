cee6e49aa13c33d8de03d211f60abfb6eb90052a
NikolajNielsen <nan@strongminds.dk>
2025-02-21 09:49:47 +0100

    Add publish controller

--- PubSub.Application/Controllers/PublishController.cs
+++ PubSub.Application/Controllers/PublishController.cs
@@ -3,8 +3,23 @@
 namespace PubSub.Application.Controllers
 {
     [ApiController]
-    [Route("")]
+    [Route("publish")]
     public class PublishController: ControllerBase
     {
+        private readonly IPublisher _publisher;
+
+        public PublishController(IPublisher publisher)
+        {
+            _publisher = publisher;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Publish(Publication publication) {
+            if (!ModelState.IsValid) return BadRequest("Invalid request object provided.");
+
+            await _publisher.Publish(publication);
+
+            return Ok();
+        }
     }
 }
