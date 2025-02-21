cc00dd87d5a923067d284c2750da0defdf299dcc
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-21 13:54:59 +0100

    initial controller and request type

---
+++ Presentation.Web/Controllers/API/V2/External/ItSystems/ItSystemDBSV2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Http;
+using Presentation.Web.Infrastructure.Attributes;
+using Presentation.Web.Models.API.V2.Request.System.Regular;
+
+namespace Presentation.Web.Controllers.API.V2.External.ItSystems;
+
+[RoutePrefix("api/v2/it-systems/{systemUuid}/dbs")]
+public class ItSystemDBSV2Controller : ExternalBaseController
+{
+    public ItSystemDBSV2Controller() {}
+
+    [Route]
+    public IHttpActionResult PatchDbsProperties([NonEmptyGuid] [FromUri] Guid systemUuid, UpdateDBSPropertiesRequestDTO request)
+    {
+        return NotFound();
+    }
+}
---
+++ Presentation.Web/Models/API/V2/Request/System/Regular/UpdateDBSPropertiesRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.Web.Models.API.V2.Request.System.Regular;
+
+public class UpdateDBSPropertiesRequestDTO
+{
+    [MaxLength(100)]
+    public string SystemName { get; set; }
+
+    [MaxLength(100)]
+    public string DataProcessorName { get; set; }
+}
