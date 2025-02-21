293f5ba99a6f4477440229a86ce57c3ee85817fd
NikolajNielsen <nan@strongminds.dk>
2025-02-21 11:16:03 +0100

    Move startup tasks

---
+++ PubSub.Application/StartupTasks/StartupTaskRunner.cs
@@ -0,0 +1,13 @@
+﻿namespace PubSub.Application.StartupTasks
+{
+    public class StartupTaskRunner
+    {
+        public static async Task RunAsync(IEnumerable<IStartupTask> startupTasks)
+        {
+            foreach (var task in startupTasks)
+            {
+                await task.ExecuteAsync();
+            }
+        }
+    }
+}
