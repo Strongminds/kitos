23c1cbd1e9753cc4f9c7c79956ff4bfabb90414f
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-24 10:59:14 +0100

    extend api model and mapper

--- Tests.Integration.Presentation.Web/ItSystem/V2/ItSystemDBSV2ApiTest.cs
+++ Tests.Integration.Presentation.Web/ItSystem/V2/ItSystemDBSV2ApiTest.cs
@@ -1,4 +1,9 @@
 ﻿using System.Threading.Tasks;
+using Core.DomainModel;
+using Core.DomainModel.Organization;
+using Presentation.Web.Models.API.V2.Request.System.Regular;
+using Tests.Integration.Presentation.Web.Tools;
+using Tests.Integration.Presentation.Web.Tools.External;
 using Xunit;
 
 namespace Tests.Integration.Presentation.Web.ItSystem.V2
@@ -7,9 +12,18 @@ namespace Tests.Integration.Presentation.Web.ItSystem.V2
     {
 
         [Fact]
-        public static async Task Can_Update_DBS_Name()
+        public async Task Can_Update_DBS_Name()
         {
+            var token = await HttpApi.GetTokenAsync(OrganizationRole.GlobalAdmin);
+            var org = await CreateOrganizationAsync();
+            var (systemUuid, _) = await CreateSystemAsync(org.Id, AccessModifier.Public);
+            var request = new UpdateDBSPropertiesRequestDTO { SystemName = A<string>() };
 
+            var response = await DBSV2Helper.PatchSystem(systemUuid, request);
+
+            Assert.True(response.IsSuccessStatusCode);
+            var systemAfter = await ItSystemV2Helper.GetSingleAsync(token.Token, systemUuid);
+            Assert.Equal(request.SystemName, systemAfter.DBSName);
         } 
     }
 }
