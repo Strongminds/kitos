056f086dd3a1924f1093fc971f9d6328350b34c1
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-24 10:19:01 +0100

    add test file and helper for dbs

---
+++ Tests.Integration.Presentation.Web/ItSystem/V2/ItSystemDBSV2ApiTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Integration.Presentation.Web.ItSystem.V2
+{
+    public class ItSystemDBSV2ApiTest : BaseItSystemsApiV2Test
+    {
+
+        [Fact]
+        public static async Task Can_Update_DBS_Name()
+        {
+
+        } 
+    }
+}
---
+++ Tests.Integration.Presentation.Web/Tools/External/DBSV2Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Core.DomainModel.Organization;
+using Presentation.Web.Models.API.V2.Request.System.Regular;
+
+namespace Tests.Integration.Presentation.Web.Tools.External
+{
+    public class DBSV2Helper
+    {
+        private const string ControllerRoutePrefix = "api/v2/it-systems";
+
+        public static async Task<HttpResponseMessage> PatchSystem(Guid systemUuid,
+            UpdateDBSPropertiesRequestDTO request)
+        {
+            var cookie = await HttpApi.GetCookieAsync(OrganizationRole.GlobalAdmin);
+            var url = GetUrl(systemUuid);
+            return await HttpApi.PostWithCookieAsync(url, cookie, request);
+        }
+
+        private static Uri GetUrl(Guid systemUuid)
+        {
+            return TestEnvironment.CreateUrl($"{ControllerRoutePrefix}/{systemUuid}/dbs");
+        }
+    }
+}
