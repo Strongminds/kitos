16c521844c77bbd10e19721cdff46c084c9f4cff
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-24 09:59:46 +0100

    add migration files

---
+++ Infrastructure.DataAccess/Migrations/202502240858588_AddIndexesToDBSFields.cs
@@ -0,0 +1,20 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIndexesToDBSFields : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.ItSystem", "DBSName", name: "ItSystem_IX_DBSName");
+            CreateIndex("dbo.ItSystem", "DBSDataProcessorName", name: "ItSystem_IX_DBSDataProcessorName");
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.ItSystem", "ItSystem_IX_DBSDataProcessorName");
+            DropIndex("dbo.ItSystem", "ItSystem_IX_DBSName");
+        }
+    }
+}
