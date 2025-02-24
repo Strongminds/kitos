7a9715c6f60d9263b1d2bc14dfef0e1e8e279be0
JacobBorchMadsen <jbma@strongminds.dk>
2025-02-24 09:44:02 +0100

    remove unneeded file

--- Core.ApplicationServices/IITSystemUpdater.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Abstractions.Types;
-using Core.DomainModel.ItSystem;
-
-namespace Core.ApplicationServices;
-
-public interface IItSystemUpdater
-{
-    Result<ItSystem, OperationError> UpdateItSystem(ItSystem system);
-}
