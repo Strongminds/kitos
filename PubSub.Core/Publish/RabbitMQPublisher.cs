d6b00d486e8ced10512ef6fe3f188cb1c4c5eef0
NikolajNielsen <nan@strongminds.dk>
2025-02-26 06:47:44 +0100

    Add publish to git

---
+++ PubSub.Core/Publish/RabbitMQPublisher.cs
@@ -0,0 +1,31 @@
+﻿using PubSub.Application.Common;
+using RabbitMQ.Client;
+
+namespace PubSub.Application.Publish
+{
+    public class RabbitMQPublisher : IPublisher
+    {
+        private readonly IMessageSerializer _messageSerializer;
+        private readonly IChannel _channel;
+        private readonly IMessageBusTopicManager _messageBusTopicManager;
+
+        public RabbitMQPublisher(IMessageSerializer messageSerializer, IChannel channel, IMessageBusTopicManager messageBusTopicManager)
+        {
+            _messageSerializer = messageSerializer;
+            _channel = channel;
+            _messageBusTopicManager = messageBusTopicManager;
+        }
+
+        public async Task Publish(Publication publication)
+        {
+            var topic = publication.Queue;
+            var message = publication.Message;
+
+            await _messageBusTopicManager.Add(new Topic { Name = topic });
+            var serializedBody = _messageSerializer.Serialize(message);
+
+            await _channel.BasicPublishAsync(exchange: string.Empty, routingKey: topic, body: serializedBody);
+            Console.WriteLine($"Published {message}");
+        }
+    }
+}
