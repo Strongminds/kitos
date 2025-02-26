98d3697f83bdccd866c55c5cdc75558a193850b2
NikolajNielsen <nan@strongminds.dk>
2025-02-26 13:46:14 +0100

    Consumer test

---
+++ PubSub.Test/Unit/Core/RabbitMQConsumerTest.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture;
+using Moq;
+using PubSub.Core.Consumers;
+using PubSub.Core.Managers;
+using PubSub.Core.Services.Notifier;
+using RabbitMQ.Client;
+using System.Threading.Tasks;
+
+namespace PubSub.Test.Unit.Core
+{
+    public class RabbitMQConsumerTest
+    {
+        [Fact]
+        public async Task Can_Consume_On_Channeæ()
+        {
+            var fixture = new Fixture();
+            var mockConnectionManager = new Mock<IConnectionManager>();
+            var connection = new Mock<IConnection>();
+            var channel = new Mock<IChannel>();
+            mockConnectionManager.Setup(_ => _.GetConnectionAsync()).ReturnsAsync(connection.Object);
+            connection.Setup(_ => _.CreateChannelAsync(null, default)).ReturnsAsync(channel.Object);
+
+            var mockSubscriberNotifierService = new Mock<ISubscriberNotifierService>();
+            var topic = fixture.Create<string>();
+
+            var sut = new RabbitMQConsumer(
+                mockConnectionManager.Object,
+                mockSubscriberNotifierService.Object,
+                topic
+            );
+
+            await sut.StartListeningAsync();
+
+            channel.Verify(ch => ch.BasicConsumeAsync(
+                It.IsAny<string>(),             
+                It.IsAny<bool>(),               
+                It.IsAny<string>(),             
+                It.IsAny<bool>(),               
+                It.IsAny<bool>(),            
+                It.IsAny<IDictionary<string, object?>>(), 
+                It.IsAny<IAsyncBasicConsumer>(),
+                It.IsAny<CancellationToken>()
+            ), Times.Once);
+        }
+    }
+}
