3961c71e5e5919316ec15b3e379c4fc439e8e174
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-05 13:20:57 +0100

    readd bearer token config file

---
+++ Core.ApplicationServices/Model/Authentication/BearerTokenConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using SecurityKey = Microsoft.IdentityModel.Tokens.SecurityKey;
+
+namespace Core.ApplicationServices.Model.Authentication
+{
+    public class BearerTokenConfig
+    {
+        public static string Issuer => "TODO";
+
+        public static SecurityKey SecurityKey =>
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    global::System.Web.Configuration.WebConfigurationManager.AppSettings["SecurityKeyString"]
+                )
+            );
+    }
+}
