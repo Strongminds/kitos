9b20bab2672ca6f46cc574f3d2a4d9f9bc7d8457
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-05 09:47:04 +0100

    add http abstraction for infrastructure layer

--- Core.ApplicationServices/KitosEventPublisherService.cs
+++ Core.ApplicationServices/KitosEventPublisherService.cs
@@ -1,17 +1,18 @@
-﻿using System.Net.Http;
-using System.Text;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Core.Abstractions.Types;
-using Newtonsoft.Json;
+using Core.ApplicationServices.Model.KitosEvents;
+using Infrastructure.Services.Http;
 
 namespace Core.ApplicationServices
 {
     public class KitosEventPublisherService : IKitosEventPublisherService
     {
-        private readonly HttpClient _httpClient;
+        private readonly IKitosHttpClient _httpClient;
         private const string PublishUrl = "api/something";
 
-        public KitosEventPublisherService(HttpClient httpClient)
+        public KitosEventPublisherService(IKitosHttpClient httpClient)
         {
             _httpClient = httpClient;
         }
@@ -22,38 +23,10 @@ namespace Core.ApplicationServices
 
         private async Task<Maybe<OperationError>> PostEventAsync(KitosEvent eventSomething)
         {
-            var topic = eventSomething.Topic;
-            var body = ToPublicationDTO(eventSomething.EventBody);
-            var serialized = JsonConvert.SerializeObject(body);
-            return await PostRawContents(topic, serialized);
-        }
-
-        private async Task<Maybe<OperationError>> PostRawContents(string topic, string message)
-        {
-            // Create the payload according to the expected DTO structure.
-            var payloadObject = new
-            {
-                Topic = topic,
-                Message = message
-            };
-            var payloadJson = JsonConvert.SerializeObject(payloadObject);
-
-            var response = await PostAsync(payloadJson);
+            var body = new KitosEventDTO(eventSomething);
+            var url = new Uri(PublishUrl);
+            await _httpClient.PostAsync(body, url);
             return Maybe<OperationError>.None;
-        }
-
-        private async Task<HttpResponseMessage> PostAsync(string serialized)
-        {
-            var content = new StringContent(serialized, Encoding.UTF8, "application/json");
-
-            var response = await _httpClient.PostAsync(PublishUrl, content);
-            return response;
-        }
-
-        private object ToPublicationDTO(IEvent eventSomething)
-        {
-            //TODO: Convert Application level models to DTO models
-            return eventSomething;
         }
     }
 }
