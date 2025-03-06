259a171855b8e41f4a26e0bda8c23f22c97959d0
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-06 15:21:32 +0100

    add empty impl for kitos http client

---
+++ Infrastructure.Services/Http/KitosHttpClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services.Http;
+
+public class KitosHttpClient : IKitosHttpClient
+{
+    public Task<HttpResponseMessage> PostAsync(object content, Uri uri, string token)
+    {
+        throw new NotImplementedException();
+    }
+}
