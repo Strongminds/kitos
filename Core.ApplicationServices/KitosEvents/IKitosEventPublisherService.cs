559dd4276ac7d8e8ce506ade4a9368a165c5f1ad
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-06 15:13:37 +0100

    add event publisher interface

---
+++ Core.ApplicationServices/KitosEvents/IKitosEventPublisherService.cs
@@ -0,0 +1,10 @@
+﻿using Core.Abstractions.Types;
+using Core.ApplicationServices.Model.KitosEvents;
+using System.Threading.Tasks;
+
+namespace Core.ApplicationServices.KitosEvents;
+
+public interface IKitosEventPublisherService
+{
+    Task<Maybe<OperationError>> PublishEvent(KitosEvent eventSomething);
+}
