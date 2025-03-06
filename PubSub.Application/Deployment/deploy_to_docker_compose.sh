5e12d397e9eead5022f8338740eaecf9417b80aa
NikolajNielsen <nan@strongminds.dk>
2025-03-06 09:57:15 +0100

    First shot at deployment script

---
+++ PubSub.Application/Deployment/deploy_to_docker_compose.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/bash
+
+ENV=$1
+JSON_FILE="../appsettings.${ENV}.json"
+
+if [ ! -f "$JSON_FILE" ]; then
+  echo "File $JSON_FILE not found!"
+  exit 1
+fi
+
+IDP_HOST_MAPPING=$(jq -r '.IdpHostMapping' "$JSON_FILE")
+echo $IDP_HOST_MAPPING
+export IDP_HOST_MAPPING
+
+cd ..
+docker compose up
