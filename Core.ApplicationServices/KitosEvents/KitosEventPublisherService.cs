98f368d7710fbe1f218bd51579bfab6b1b05bc89
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-06 15:14:24 +0100

    add event publisher impl

---
+++ Core.ApplicationServices/KitosEvents/KitosEventPublisherService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Abstractions.Types;
+using Core.ApplicationServices.Model.KitosEvents;
+using System.Threading.Tasks;
+
+namespace Core.ApplicationServices.KitosEvents;
+
+public class KitosEventPublisherService : IKitosEventPublisherService
+{
+    private readonly IHttpEventPublisher _httpClient;
+
+    public KitosEventPublisherService(IHttpEventPublisher httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<Maybe<OperationError>> PublishEvent(KitosEvent eventSomething)
+    {
+        var dto = new KitosEventDTO(eventSomething);
+        await _httpClient.PostEventAsync(dto);
+        return Maybe<OperationError>.None;
+    }
+}
