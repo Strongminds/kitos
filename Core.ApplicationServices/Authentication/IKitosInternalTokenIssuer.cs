beb26aaf75e9697e65f30a82860fccfa7893bf0f
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-06 14:36:49 +0100

    add token issuer files

---
+++ Core.ApplicationServices/Authentication/IKitosInternalTokenIssuer.cs
@@ -0,0 +1,8 @@
+﻿using Core.ApplicationServices.Model.Authentication;
+
+namespace Core.ApplicationServices.Authentication;
+
+public interface IKitosInternalTokenIssuer
+{
+    KitosApiToken GetToken();
+}
---
+++ Core.ApplicationServices/Authentication/KitosInternalTokenIssuer.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Core.ApplicationServices.Model.Authentication;
+using Core.DomainModel;
+using Core.DomainServices.Queries.User;
+
+namespace Core.ApplicationServices.Authentication;
+
+public class KitosInternalTokenIssuer : IKitosInternalTokenIssuer
+{
+    private readonly IUserService _userService;
+    private readonly ITokenValidator _tokenValidator;
+
+    public KitosInternalTokenIssuer(IUserService userService, ITokenValidator tokenValidator)
+    {
+        _userService = userService;
+        _tokenValidator = tokenValidator;
+    }
+
+    public KitosApiToken GetToken()
+    {
+        var globalAdminUser = GetGlobalAdminUser();
+        return _tokenValidator.CreateToken(globalAdminUser);
+    }
+
+    private User GetGlobalAdminUser()
+    {
+        return _userService.GetUsers(new QueryByGlobalAdmin()).FirstOrDefault();
+    }
+}
