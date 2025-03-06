e2a08b1be7f14fc111796030811d7d157f112102
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-06 15:15:23 +0100

    add domain eventhandler for system changes

---
+++ Core.ApplicationServices/KitosEvents/PublishSystemChangeEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using Core.ApplicationServices.Model.KitosEvents;
+using Core.DomainModel.Events;
+using Core.DomainModel.ItSystem;
+
+namespace Core.ApplicationServices.KitosEvents;
+
+public class PublishSystemChangesEventHandler : IDomainEventHandler<EntityUpdatedEvent<ItSystem>>
+{
+    private readonly IKitosEventPublisherService _eventPublisher;
+    private const string SystemQueueTopic = "KitosITSystemChangedEvent";
+
+    public PublishSystemChangesEventHandler(IKitosEventPublisherService eventPublisher)
+    {
+        _eventPublisher = eventPublisher;
+    }
+    public void Handle(EntityUpdatedEvent<ItSystem> domainEvent)
+    {
+        var system = domainEvent.Entity;
+        var eventBody = new SystemChangeEventModel() { SystemName = system.Name, SystemUuid = system.Uuid };
+        var newEvent = new KitosEvent(eventBody, SystemQueueTopic);
+        _eventPublisher.PublishEvent(newEvent);
+    }
+}
