26b7fffa22e47a7c678f73669183b98b1ef232a5
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-06 15:06:57 +0100

    add kitos event dto

---
+++ Core.ApplicationServices/Model/KitosEvents/KitosEventDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.ApplicationServices.Model.KitosEvents;
+
+public class KitosEventDTO
+{
+    public KitosEventDTO(KitosEvent eventSomething)
+    {
+        Message = eventSomething.EventBody;
+        Topic = eventSomething.Topic;
+    }
+    public object Message { get; }
+    public string Topic { get; }
+}
