45f198a3a90df3c1edad31647584e0f98ed89440
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-06 15:12:09 +0100

    add impl for http event publisher

---
+++ Core.ApplicationServices/KitosEvents/HttpEventPublisher.cs
@@ -0,0 +1,29 @@
+﻿using Core.ApplicationServices.Authentication;
+using Core.ApplicationServices.Model.KitosEvents;
+using Infrastructure.Services.Http;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace Core.ApplicationServices.KitosEvents;
+
+public class HttpEventPublisher : IHttpEventPublisher
+{
+    private readonly IKitosHttpClient _httpClient;
+    private readonly IKitosInternalTokenIssuer _tokenIssuer;
+    private readonly string _baseUrl;
+    private const string PublishEndpoint = "api/publish";
+    public HttpEventPublisher(IKitosHttpClient httpClient, IKitosInternalTokenIssuer tokenIssuer, string baseUrl)
+    {
+        _httpClient = httpClient;
+        _baseUrl = baseUrl;
+        _tokenIssuer = tokenIssuer;
+    }
+
+    public Task<HttpResponseMessage> PostEventAsync(KitosEventDTO eventDTO)
+    {
+        var token = _tokenIssuer.GetToken();
+        var url = new Uri(new Uri(_baseUrl), PublishEndpoint);
+        return _httpClient.PostAsync(eventDTO, url, token.Value);
+    }
+}
