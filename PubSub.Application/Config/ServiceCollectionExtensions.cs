ca07bf8f28ede434250ae436e4a06f9412f01272
NikolajNielsen <nan@strongminds.dk>
2025-03-07 12:56:53 +0100

    Refactor extract config factory setup

--- PubSub.Application/Config/ServiceCollectionExtensions.cs
+++ PubSub.Application/Config/ServiceCollectionExtensions.cs
@@ -10,18 +10,22 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddRabbitMQ(this IServiceCollection services, ConfigurationManager configuration)
     {
-        var rabbitMQAppSettings = configuration.GetSection(Constants.Config.MessageBus.ConfigSection);
-        var hostName = rabbitMQAppSettings.GetValue<string>(Constants.Config.MessageBus.HostName) ?? throw new ArgumentNullException("No RabbitMQ host name found in settings.");
-        var user = configuration.GetValue<string>(Constants.Config.MessageBus.User) ?? throw new ArgumentNullException("No RabbitMQ username found in settings.");
-        var password = configuration.GetValue<string>(Constants.Config.MessageBus.Password) ?? throw new ArgumentNullException("No RabbitMQ password found in settings.");
-        var connectionFactory = new ConnectionFactory { HostName = hostName, UserName = user, Password = password };
-        
+        var connectionFactory = GetConnectionFactory(configuration);
         services.AddSingleton<IConnectionFactory>(_ => connectionFactory);
         services.AddSingleton<ISubscriberService, RabbitMQSubscriberService>();
         services.AddSingleton<IConnectionManager, RabbitMQConnectionManager>();
         services.AddSingleton<IPublisherService, RabbitMQPublisherService>();
 
         return services;
+    }
+
+    private static IConnectionFactory GetConnectionFactory(IConfiguration configuration)
+    {
+        var rabbitMQAppSettings = configuration.GetSection(Constants.Config.MessageBus.ConfigSection);
+        var hostName = rabbitMQAppSettings.GetValue<string>(Constants.Config.MessageBus.HostName) ?? throw new ArgumentNullException("No RabbitMQ host name found in settings.");
+        var user = configuration.GetValue<string>(Constants.Config.MessageBus.User) ?? throw new ArgumentNullException("No RabbitMQ username found in settings.");
+        var password = configuration.GetValue<string>(Constants.Config.MessageBus.Password) ?? throw new ArgumentNullException("No RabbitMQ password found in settings.");
+        return new ConnectionFactory { HostName = hostName, UserName = user, Password = password };
     }
 
     public static IServiceCollection AddRequestMapping(this IServiceCollection services)
