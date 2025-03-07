061a200a5debcd2d0dc342a934e15c5c3e74f595
NikolajNielsen <nan@strongminds.dk>
2025-03-07 11:31:27 +0100

    Fix rabbit exports and export env

--- PubSub.Application/Deployment/deploy_to_docker_compose.sh
+++ PubSub.Application/Deployment/deploy_to_docker_compose.sh
@@ -1,9 +1,10 @@
 ﻿#!/bin/bash
 
 #SETUP ENVIRONMENT VARIABLES
-ENV=$1
-JSON_FILE="../appsettings.${ENV}.json"
+ASPNETCORE_ENVIRONMENT=$1
+export ASPNETCORE_ENVIRONMENT
 
+JSON_FILE="../appsettings.${ASPNETCORE_ENVIRONMENT}.json"
 if [ ! -f "$JSON_FILE" ]; then
   echo "File $JSON_FILE not found!"
   exit 1
@@ -11,11 +12,10 @@ fi
 
 IDP_HOST_MAPPING=$(jq -r '.IdpHostMapping' "$JSON_FILE")
 export IDP_HOST_MAPPING
-
 RABBIT_MQ_USER=$2
 RABBIT_MQ_PASSWORD=$3
-export $RABBIT_MQ_USER
-export $RABBIT_MQ_PASSWORD
+export RABBIT_MQ_USER
+export RABBIT_MQ_PASSWORD
 
 #RUN DOCKER COMPOSE
 cd ..
