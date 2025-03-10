be24fb1d6eea5c52cba48aeed0d977eae1ac5178
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-10 09:46:17 +0100

    add integration test files

---
+++ Tests.Integration.Presentation.Web/KitosEvents/ItSystemChangedEventsApiV2Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Tests.Integration.Presentation.Web.Tools;
+using Xunit;
+
+namespace Tests.Integration.Presentation.Web.KitosEvents
+{
+    public class ItSystemChangedEventsApiV2Test
+    {
+        [Fact]
+        public Task Can_Publish_Event()
+        {
+            var request = new { };
+
+            HttpApi.PostAsync()
+        }
+    }
+}
