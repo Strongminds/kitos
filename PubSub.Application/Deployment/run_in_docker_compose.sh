b5ca139a000430e1b13f78729395f2298534ef16
NikolajNielsen <nan@strongminds.dk>
2025-03-18 19:38:57 +0100

    Move idp host mapping out of appsettings since it is only relevant to docker-compose

--- PubSub.Application/Deployment/run_in_docker_compose.sh
+++ PubSub.Application/Deployment/run_in_docker_compose.sh
@@ -4,14 +4,6 @@
 ASPNETCORE_ENVIRONMENT=$1
 export ASPNETCORE_ENVIRONMENT
 
-JSON_FILE="appsettings.${ASPNETCORE_ENVIRONMENT}.json"
-if [ ! -f "$JSON_FILE" ]; then
-  echo "File $JSON_FILE not found!"
-  exit 1
-fi
-
-IDP_HOST_MAPPING=$(jq -r '.IdpHostMapping' "$JSON_FILE")
-export IDP_HOST_MAPPING
 RABBIT_MQ_USER=$2
 RABBIT_MQ_PASSWORD=$3
 export RABBIT_MQ_USER
@@ -19,6 +11,9 @@ export RABBIT_MQ_PASSWORD
 
 PUBSUB_API_KEY=$4
 export PUBSUB_API_KEY
+
+IDP_HOST_MAPPING=$5
+export IDP_HOST_MAPPING
 
 #RUN DOCKER COMPOSE
 if [[ "$(uname)" == "Linux" ]]; then
