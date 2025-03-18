ee4cacd91a6cddb6475ce43cdc9ddaa43dc254da
NikolajNielsen <nan@strongminds.dk>
2025-03-18 12:42:10 +0100

    Try extracting build step to script

---
+++ PubSub.Application/Deployment/build-and-push-docker-image.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+
+#cd ../..
+if [[ "$(uname)" == "Linux" ]]; then
+	sudo docker build -t $1/$2:latest --file PubSub.Application/Dockerfile .
+    sudo docker tag $1/$2:latest $1/$2:$3
+    sudo docker push $1/$2:latest 
+    sudo docker push $1/$2:$$3
+else
+	docker build -t $1/$2:latest --file PubSub.Application/Dockerfile .
+    docker tag $1/$2:latest $1/$2:$3
+    docker push $1/$2:latest
+    docker push $1/$2:$$3
+fi
--- PubSub.Application/Deployment/build.sh
+++
@@ -1,8 +0,0 @@
-﻿#!/bin/bash
-
-cd ../..
-if [[ "$(uname)" == "Linux" ]]; then
-	sudo docker build --tag kitos-pubsub --file PubSub.Application/Dockerfile .
-else
-	docker build --tag kitos-pubsub --file PubSub.Application/Dockerfile .
-fi
