7ce5be8fc8bc94fc0e34ad36c5d420161ba2302f
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-26 13:38:28 +0100

    add new files for event types

---
+++ Core.ApplicationServices/Model/KitosEvents/SystemNameChangeEventBodyModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Abstractions.Types;
+using Core.ApplicationServices.Model.Shared;
+
+namespace Core.ApplicationServices.Model.KitosEvents;
+
+public class SystemNameChangeEventBodyModel : IEventBody
+{
+    public Guid SystemUuid { get; set; }
+    public OptionalValueChange<string> SystemName { get; set; } = OptionalValueChange<string>.None;
+    public Dictionary<string, object> ToKeyValuePairs()
+    {
+        var keyValuePairs = new Dictionary<string, object> { { nameof(SystemUuid), SystemUuid } };
+
+        if (SystemName.HasChange) keyValuePairs.Add(nameof(SystemName), SystemName.NewValue);
+
+        return keyValuePairs;
+    }
+
+    public string Type()
+    {
+        return "NameChange";
+    }
+}
