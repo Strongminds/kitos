9ce9a68250348bf115debcd27e9003b95f948943
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2025-03-30 14:33:39 +0200

    Fix for string interpolation

--- DeploymentScripts/DeployPubsubToEnvironment.ps1
+++ DeploymentScripts/DeployPubsubToEnvironment.ps1
@@ -7,10 +7,10 @@ $ErrorActionPreference = "Stop"
 
 $remoteUser = "kitosadmintest"
 $remoteHost = "10.212.74.11"
-$composeFile = "PubSub.Application\docker-compose.yml"
+$composeFile = "../PubSub.Application/docker-compose.yml"
 
 # Copy docker-compose file
-scp -i id_rsa $composeFile $remoteUser@$remoteHost:/home/kitosadmintest/app
+scp -i id_rsa $composeFile "$remoteUser@$remoteHost:/home/kitosadmintest/app"
 
 # Run remote docker compose
 ssh -i id_rsa $remoteUser@$remoteHost "cd /home/kitosadmintest/app && docker-compose pull && docker-compose up -d"
