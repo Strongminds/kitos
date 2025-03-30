10bd7bb7ef6778fb33f6f93867b7cbfa0a4e5f59
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2025-03-30 20:59:59 +0200

    Fix cat command

--- DeploymentScripts/DeployPubsubToEnvironment.ps1
+++ DeploymentScripts/DeployPubsubToEnvironment.ps1
@@ -18,7 +18,7 @@ $remoteTarget = "${remoteUser}@${remoteHost}:${remotePath}"
 
 # Copy compose-file
 Write-Host "Copying $composeFile to $remoteTarget"
-Get-Content -Path $composeFile -Raw | ssh -i $keyPath $remoteUser@$remoteHost "cat > $remotePath/docker-compose.yml"
+Get-Content -Path $composeFile -Raw | ssh -i $keyPath "$remoteUser@$remoteHost" "cat > $remotePath/docker-compose.yml"
 
 if ($LASTEXITCODE -ne 0) {
     Write-Error "SCP failed with exit code $LASTEXITCODE"
