522ccdecf39edadf5bf53b26ff931db08c176b86
Morten Hoffmann Sørensen <mhs@strongminds.dk>
2025-03-30 12:21:25 +0200

    Added TeamCity pubsub deployment script

---
+++ DeploymentScripts/DeployPubsubToEnvironment.ps1
@@ -0,0 +1,16 @@
+param(
+    [Parameter(Mandatory=$true)][string]$targetEnvironment
+    )
+
+# Stop on first error
+$ErrorActionPreference = "Stop"
+
+$remoteUser = "kitosadmintest"
+$remoteHost = "10.212.74.11"
+$composeFile = "PubSub.Application\docker-compose.yml"
+
+# Copy docker-compose file
+scp -i id_rsa $composeFile $remoteUser@$remoteHost:/home/kitosadmintest/app
+
+# Run remote docker compose
+ssh -i id_rsa $remoteUser@$remoteHost "cd /home/kitosadmintest/app && docker-compose pull && docker-compose up -d"
