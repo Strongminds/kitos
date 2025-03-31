c6feb0a3928c980c51868aa0bca85a2aefe8cee9
JacobBorchMadsen <jbma@strongminds.dk>
2025-03-31 15:24:19 +0200

    add docker login powershell file

--- PubSub.Application/Deployment/build_and_push_docker_image.sh
+++ PubSub.Application/Deployment/build_and_push_docker_image.sh
@@ -1,17 +1,14 @@
 ﻿#!/bin/bash
 
-DOCKER_USERNAME=$1
-DOCKER_REPOSITORY=$2
-DOCKER_TAG=$3
+DOCKER_REPOSITORY=$1
+DOCKER_TAG=$2
 
 if [[ "$(uname)" == "Linux" ]]; then
-    sudo docker build -t $DOCKER_USERNAME/$DOCKER_REPOSITORY:latest --file PubSub.Application/Dockerfile .
-    sudo docker tag $DOCKER_USERNAME/$DOCKER_REPOSITORY:latest $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
-    sudo docker push $DOCKER_USERNAME/$DOCKER_REPOSITORY:latest 
-    sudo docker push $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
+    sudo docker build -t $DOCKER_REPOSITORY:latest --file PubSub.Application/Dockerfile .
+    sudo docker tag $DOCKER_REPOSITORY:latest $DOCKER_REPOSITORY:$DOCKER_TAG
+    sudo docker push $DOCKER_REPOSITORY:$DOCKER_TAG
 else
-    docker build -t $DOCKER_USERNAME/$DOCKER_REPOSITORY:latest --file PubSub.Application/Dockerfile .
-    docker tag $DOCKER_USERNAME/$DOCKER_REPOSITORY:latest $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
-    docker push $DOCKER_USERNAME/$DOCKER_REPOSITORY:latest
-    docker push $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG
+    docker build -t $DOCKER_REPOSITORY:latest --file PubSub.Application/Dockerfile .
+    docker tag $DOCKER_REPOSITORY:latest $DOCKER_REPOSITORY:$DOCKER_TAG
+    docker --debug push $DOCKER_REPOSITORY:$DOCKER_TAG
 fi
