4639077945ea110d62aee0f1e32476e14801bebf
JacobBorchMadsen <jbma@strongminds.dk>
2025-04-08 15:19:01 +0200

    make db set for subscriptions and setup OnConfiguring hook

--- DataAccess/PubSubContext.cs
+++ DataAccess/PubSubContext.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PubSub.Core.Models;
 
 namespace PubSub.DataAccess
 {
     public class PubSubContext : DbContext
     {
+        public PubSubContext()
+        {
+        }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => throw new NotImplementedException();
+
+        public DbSet<Subscription> Subscriptions { get; set; }
     }
 }
