e617f64c930e92d96b40548dec2a9ad60bdce60b
JacobBorchMadsen <jbma@strongminds.dk>
2025-04-10 10:58:03 +0200

    create basic repository for subscriptions

---
+++ DataAccess/ISubscriptionRepository.cs
@@ -0,0 +1,13 @@
+﻿using CSharpFunctionalExtensions;
+using PubSub.Core.Models;
+
+namespace PubSub.DataAccess;
+
+public interface ISubscriptionRepository
+{
+    Task<IEnumerable<Subscription>> GetAllAsync();
+    Task<Maybe<Subscription>> GetAsync(Guid uuid);
+    Task AddAsync(Subscription subscription);
+    Task UpdateAsync(Subscription subscription);
+    Task DeleteAsync(Guid uuid);
+}
---
+++ DataAccess/SubscriptionRepository.cs
@@ -0,0 +1,52 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.EntityFrameworkCore;
+using PubSub.Core.Models;
+using PubSub.DataAccess.Helpers;
+
+namespace PubSub.DataAccess;
+
+public class SubscriptionRepository : ISubscriptionRepository
+{
+    private readonly PubSubContext _context;
+    public SubscriptionRepository(PubSubContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Subscription>> GetAllAsync()
+    {
+        return await _context.Subscriptions.ToListAsync();
+    }
+
+    public async Task<Maybe<Subscription>> GetAsync(Guid uuid)
+    {
+        var subscription = await _context.Subscriptions.FirstOrDefaultAsync(x => x.Uuid == uuid);
+        return Maybe<Subscription>.From(subscription);
+    }
+
+
+    public async Task AddAsync(Subscription subscription)
+    {
+        await _context.Subscriptions.AddAsync(subscription);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Subscription subscription)
+    {
+        _context.Subscriptions.Update(subscription);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid uuid)
+    {
+
+        var subscription = await GetAsync(uuid);
+        subscription.Tap(DeleteSubscription);
+    }
+
+    private async void DeleteSubscription(Subscription subscription)
+    {
+        _context.Subscriptions.Remove(subscription);
+        await _context.SaveChangesAsync();
+    }
+}
