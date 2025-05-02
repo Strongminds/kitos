1820271daa40e0404659420acd723e384d8b2aa4
JacobBorchMadsen <jbma@strongminds.dk>
2025-05-02 11:30:21 +0200

    remove unused v1 methods from user helper

--- Tests.Integration.Presentation.Web/Tools/UserHelper.cs
+++ Tests.Integration.Presentation.Web/Tools/UserHelper.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using Core.DomainModel.Organization;
+﻿using Core.DomainModel.Organization;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Presentation.Web.Models.API.V1;
-using Presentation.Web.Models.API.V1.Users;
 using Presentation.Web.Models.API.V2.Internal.Response.User;
 using Xunit;
 
@@ -50,35 +47,6 @@ namespace Tests.Integration.Presentation.Web.Tools
                 return await HttpApi.DeleteWithCookieAsync(TestEnvironment.CreateUrl(request + $"/{organizationId}"), cookie);
             }
             return await HttpApi.DeleteWithCookieAsync(TestEnvironment.CreateUrl(request), cookie);
-        }
-
-        public static async Task<List<UserWithEmailDTO>> SearchUsersAsync(string query, Cookie optionalLogin = null)
-        {
-            using var response = await SendSearchUsersAsync(query, optionalLogin);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-            return await response.ReadResponseBodyAsKitosApiResponseAsync<List<UserWithEmailDTO>>();
-        }
-
-        public static async Task<HttpResponseMessage> SendSearchUsersAsync(string query, Cookie optionalLogin = null)
-        {
-            var cookie = optionalLogin ?? await HttpApi.GetCookieAsync(OrganizationRole.GlobalAdmin);
-            return await HttpApi.GetWithCookieAsync(TestEnvironment.CreateUrl($"api/users/search?query={query}"), cookie);
-        }
-
-        public static async Task<UserAdministrationPermissionsDTO> GetUserAdministrationPermissionsAsync(Guid organizationUuid,Cookie optionalLogin = null)
-        {
-            using var response = await SendGetUserAdministrationPermissionsRequestAsync(organizationUuid,optionalLogin);
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-
-            return await response.ReadResponseBodyAsKitosApiResponseAsync<UserAdministrationPermissionsDTO>();
-        }
-
-        public static async Task<HttpResponseMessage> SendGetUserAdministrationPermissionsRequestAsync(Guid organizationUuid,Cookie optionalLogin = null)
-        {
-            var cookie = optionalLogin ?? await HttpApi.GetCookieAsync(OrganizationRole.GlobalAdmin);
-            var url = TestEnvironment.CreateUrl($"api/v1/organizations/{organizationUuid:D}/administration/users/permissions");
-            return await HttpApi.GetWithCookieAsync(url, cookie);
         }
     }
 }
