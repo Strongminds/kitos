c32bcae61ff344cba2c0a2465f257d9ffa851a9e
JacobBorchMadsen <jbma@strongminds.dk>
2025-05-07 14:08:20 +0200

    delete now unused refs and v1 helper methods

--- Tests.Integration.Presentation.Web/Tools/ItSystemUsageHelper.cs
+++ Tests.Integration.Presentation.Web/Tools/ItSystemUsageHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Core.DomainModel.ItSystem;
 using Core.DomainModel.ItSystemUsage.GDPR;
 using Core.DomainModel.ItSystemUsage.Read;
 using Core.DomainModel.Organization;
@@ -131,34 +130,6 @@ namespace Tests.Integration.Presentation.Web.Tools
                     new { } // No body for this call
                     );
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            return response;
-        }
-
-        public static async Task<ArchivePeriod> AddArchivePeriodAsync(int systemUsageId, DateTime startDate, DateTime endDate, int organizationId, Cookie optionalLogin = null)
-        {
-            var cookie = optionalLogin ?? await HttpApi.GetCookieAsync(OrganizationRole.GlobalAdmin);
-            var url = TestEnvironment.CreateUrl($"odata/ArchivePeriods?organizationId={organizationId}");
-            var body = new
-            {
-                ItSystemUsageId = systemUsageId,
-                StartDate = startDate,
-                EndDate = endDate
-            };
-            using var response = await HttpApi.PostWithCookieAsync(url, cookie, body);
-            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
-            return await response.ReadResponseBodyAsAsync<ArchivePeriod>();
-        }
-
-        public static async Task<HttpResponseMessage> SetIsHoldingDocumentRequestAsync(int systemUsageId, bool isDocumentHolding, Cookie optionalLogin = null)
-        {
-            var cookie = optionalLogin ?? await HttpApi.GetCookieAsync(OrganizationRole.GlobalAdmin);
-            var url = TestEnvironment.CreateUrl($"odata/ItSystemUsages({systemUsageId})");
-            var body = new
-            {
-                Registertype = isDocumentHolding
-            };
-            using var response = await HttpApi.PatchWithCookieAsync(url, cookie, body);
-            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
             return response;
         }
     }
