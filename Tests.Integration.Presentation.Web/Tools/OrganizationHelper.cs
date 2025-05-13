139ce4d4512fb93084a1925b7109f714099a7911
JacobBorchMadsen <jbma@strongminds.dk>
2025-05-13 11:02:13 +0200

    remove unused

--- Tests.Integration.Presentation.Web/Tools/OrganizationHelper.cs
+++ Tests.Integration.Presentation.Web/Tools/OrganizationHelper.cs
@@ -1,11 +1,8 @@
-﻿using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using AutoFixture;
 using Core.DomainModel;
 using Core.DomainModel.Organization;
-using Core.DomainServices.Extensions;
 using Presentation.Web.Models.API.V1;
 using Xunit;
 
@@ -34,29 +31,6 @@ namespace Tests.Integration.Presentation.Web.Tools
             };
 
             return await HttpApi.PostWithCookieAsync(url, cookie, body);
-        }
-
-        public static async Task<OrgUnitDTO> CreateOrganizationUnitAsync(int organizationId, string orgUnitName, int? parentId = null, Cookie optionalLogin = null)
-        {
-            var cookie = optionalLogin ?? await HttpApi.GetCookieAsync(OrganizationRole.GlobalAdmin);
-            var url = TestEnvironment.CreateUrl("api/organizationUnit/");
-            var fixture = new Fixture();
-            var parentUnitId =
-                parentId ??
-                //Fallback to the root of the organization
-                DatabaseAccess.MapFromEntitySet<OrganizationUnit, int>(units => units.AsQueryable().ByOrganizationId(organizationId).First(x => x.ParentId == null).Id);
-            var body = new
-            {
-                name = orgUnitName,
-                organizationId = organizationId,
-                parentId = parentUnitId,
-                ean = fixture.Create<uint>() % 10000,
-                localId = fixture.Create<string>()
-            };
-
-            using var createdResponse = await HttpApi.PostWithCookieAsync(url, cookie, body);
-            Assert.Equal(HttpStatusCode.Created, createdResponse.StatusCode);
-            return await createdResponse.ReadResponseBodyAsKitosApiResponseAsync<OrgUnitDTO>();
         }
     }
 }
