d1b88963b05bece1baf8276bcc3acac88afd37be
NikolajNielsen <nan@strongminds.dk>
2025-05-15 09:57:30 +0200

    Add extra step to gss init

--- PubSub.Application.Api/Program.cs
+++ PubSub.Application.Api/Program.cs
@@ -3,6 +3,7 @@ using PubSub.Application.Api;
 using PubSub.Infrastructure.DataAccess;
 using PubSub.Application.Api.Configuration;
 using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.DataProtection;
 
 
 [DllImport("System.Net.Security.Native", EntryPoint = "NetSecurityNative_EnsureGssInitialized")]
@@ -39,6 +40,8 @@ builder.WebHost.ConfigureKestrel((context, options) =>
         }
     });
 });
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(@"/opt/app-root/.aspnet/DataProtection-Keys"));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
