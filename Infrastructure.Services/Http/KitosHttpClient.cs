ab24ddb7b9d39561e377dfd083030506ce37bec8
JacobBorchMadsen <jbma@strongminds.dk>
2025-05-22 12:51:10 +0200

    add sleep statement for debbugging staging env

--- Infrastructure.Services/Http/KitosHttpClient.cs
+++ Infrastructure.Services/Http/KitosHttpClient.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Serilog;
@@ -36,6 +37,7 @@ public class KitosHttpClient : IKitosHttpClient
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
         try
         {
+            Thread.Sleep(TimeSpan.FromMinutes(2));
             var debug = await _httpClient.SendAsync(request);
             var toLog = await debug.Content.ReadAsStringAsync();
             _logger.Fatal("Response from httpClient in KitosHttpClient: " + toLog + " with statuscode: " + debug.StatusCode);
