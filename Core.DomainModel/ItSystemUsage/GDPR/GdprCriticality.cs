3dc072bdda80e52dffdb8383391d7880ae3140be
NikolajNielsen <nan@strongminds.dk>
2025-09-24 07:54:33 +0200

    Add real enum

--- Core.DomainModel/ItSystemUsage/GDPR/GdprCriticality.cs
+++ Core.DomainModel/ItSystemUsage/GDPR/GdprCriticality.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.DomainModel.ItSystemUsage.GDPR
 {
-    class GdprCriticality
+    public enum GdprCriticality
     {
+        NOT_CRITICAL = 0,
+        LOW = 1,
+        MEDIUM = 2,
+        HIGH = 3,
+        VERY_HIGH = 4
+    }
+
+    public static class GdprCriticalityExtensions
+    {
+        public static string GetReadableName(this GdprCriticality gdprCriticality)
+        {
+            return gdprCriticality switch
+            {
+                GdprCriticality.NOT_CRITICAL => "Ikke kritisk",
+                GdprCriticality.LOW => "Lav",
+                GdprCriticality.MEDIUM => "Medium",
+                GdprCriticality.HIGH => "Høj",
+                GdprCriticality.VERY_HIGH => "Meget høj",
+                _ => throw new InvalidOperationException($"Invalid gdprCriticality value: {gdprCriticality}")
+            };
+        }
     }
 }
