e1287a012ef540952c2d0383c3bfc707dd064208
NikolajNielsen <nan@strongminds.dk>
2025-09-29 11:35:38 +0200

    Add migration

---
+++ Infrastructure.DataAccess/Migrations/202509290929471_DPR_OversightReportLinks.cs
@@ -0,0 +1,24 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DPR_OversightReportLinks : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.DataProcessingRegistrationReadModels", "LatestOversightReportLink", c => c.String());
+            AddColumn("dbo.DataProcessingRegistrationReadModels", "LatestOversightReportLinkName", c => c.String());
+            AddColumn("dbo.DataProcessingRegistrationOversightDates", "OversightReportLink", c => c.String());
+            AddColumn("dbo.DataProcessingRegistrationOversightDates", "OversightReportLinkName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.DataProcessingRegistrationOversightDates", "OversightReportLinkName");
+            DropColumn("dbo.DataProcessingRegistrationOversightDates", "OversightReportLink");
+            DropColumn("dbo.DataProcessingRegistrationReadModels", "LatestOversightReportLinkName");
+            DropColumn("dbo.DataProcessingRegistrationReadModels", "LatestOversightReportLink");
+        }
+    }
+}
