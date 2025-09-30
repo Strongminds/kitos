4e74d8704a0bbcb83c67e1b8b7fe778273f2586e
NikolajNielsen <nan@strongminds.dk>
2025-09-30 11:00:43 +0200

    Remove obsolete function

--- Core.DomainModel/ItSystemUsage/GDPR/GdprCriticality.cs
+++ Core.DomainModel/ItSystemUsage/GDPR/GdprCriticality.cs
@@ -10,20 +10,4 @@ namespace Core.DomainModel.ItSystemUsage.GDPR
         High = 3,
         VeryHigh = 4
     }
-
-    public static class GdprCriticalityExtensions
-    {
-        public static string GetReadableName(this GdprCriticality gdprCriticality)
-        {
-            return gdprCriticality switch
-            {
-                GdprCriticality.NotCritical => "Ikke kritisk",
-                GdprCriticality.Low => "Lav",
-                GdprCriticality.Medium => "Medium",
-                GdprCriticality.High => "Høj",
-                GdprCriticality.VeryHigh => "Meget høj",
-                _ => throw new InvalidOperationException($"Invalid gdprCriticality value: {gdprCriticality}")
-            };
-        }
-    }
 }
