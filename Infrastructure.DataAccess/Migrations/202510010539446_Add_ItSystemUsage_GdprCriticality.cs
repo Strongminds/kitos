319b816656c3d01d840f9e05e90ed4164dd97c33
Aleksander Naskret <aln@strongminds.dk>
2025-10-01 07:42:41 +0200

    fix migration

---
+++ Infrastructure.DataAccess/Migrations/202510010539446_Add_ItSystemUsage_GdprCriticality.cs
@@ -0,0 +1,20 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_ItSystemUsage_GdprCriticality : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ItSystemUsage", "GdprCriticality", c => c.Int());
+            AddColumn("dbo.ItSystemUsageOverviewReadModels", "GdprCriticality", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ItSystemUsageOverviewReadModels", "GdprCriticality");
+            DropColumn("dbo.ItSystemUsage", "GdprCriticality");
+        }
+    }
+}
