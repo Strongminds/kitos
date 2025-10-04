33ba14df7325fa599086ec2628e2abb590ae6dcf
NikolajNielsen <nan@strongminds.dk>
2025-10-04 16:01:51 +0200

    Add inverse tests to confirm that the two checks don't interfere with each other's fields

--- Tests.Unit.Presentation.Web/Authorization/SupplierAssociatedFieldsServiceTest.cs
+++ Tests.Unit.Presentation.Web/Authorization/SupplierAssociatedFieldsServiceTest.cs
@@ -56,6 +56,25 @@ namespace Tests.Unit.Presentation.Web.Authorization
         }
 
         [Theory]
+        [InlineData(true, false, false, false)]
+        public void GivenChangesToAnySupplierAssociatedField_RequestsChangesToNonSupplierAssociatedFields_ReturnsFalse(bool checkIsOversightCompleted, bool checkOversightDate, bool checkOversightNotes, bool checkOversightReportLink)
+        {
+            var oversight = new UpdatedDataProcessingRegistrationOversightDataParameters();
+
+            if (checkIsOversightCompleted)
+                oversight.IsOversightCompleted = A<YesNoUndecidedOption?>().AsChangedValue();
+            //TODO same as above, awaiting response from MIOL about what fields to target
+
+            var parameters = new DataProcessingRegistrationModificationParameters()
+            {
+                Oversight = oversight
+            };
+            var result = _sut.RequestsChangesToNonSupplierAssociatedFields(parameters, _dprUuid);
+
+            Assert.False(result);
+        }
+
+        [Theory]
         [InlineData(true, false, false, false, false, false)]
         [InlineData(false, true, false, false, false, false)]
         [InlineData(false, false, true, false, false, false)]
@@ -103,7 +122,57 @@ namespace Tests.Unit.Presentation.Web.Authorization
             var result = _sut.RequestsChangesToNonSupplierAssociatedFields(parameters, _dprUuid);
 
             Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(true, false, false, false, false, false)]
+        [InlineData(false, true, false, false, false, false)]
+        [InlineData(false, false, true, false, false, false)]
+        [InlineData(false, false, false, true, false, false)]
+        [InlineData(false, false, false, false, true, false)]
+        [InlineData(false, false, false, false, false, true)]
+        public void GivenChangesToANonSupplierAssociatedField_RequestsChangesToSupplierAssociatedFields_ReturnsFalse(bool addChangeToName, bool addChangeToGeneral,
+            bool addChangeToSystemUsageUuids, bool addNonSupplierChangeToOversight, bool addChangeToRoles, bool addChangeToExternalReferences)
+        {
+            var parameters = new DataProcessingRegistrationModificationParameters();
+            if (addChangeToName)
+            {
+                parameters.Name = A<string>().AsChangedValue();
             }
+            if (addChangeToGeneral)
+            {
+                parameters.General = Maybe<UpdatedDataProcessingRegistrationGeneralDataParameters>.Some(new UpdatedDataProcessingRegistrationGeneralDataParameters());
+                parameters.General.Value.AgreementConcludedAt = A<DateTime?>().AsChangedValue();
+            }
+            if (addChangeToSystemUsageUuids)
+            {
+                var existingSystemUsages = new List<ItSystemUsage>() { new() { Uuid = A<Guid>() }, new() { Uuid = A<Guid>() } };
+                _existingDpr.SystemUsages = existingSystemUsages;
+                parameters.SystemUsageUuids = Maybe<IEnumerable<Guid>>.Some(Many<Guid>());
+            }
+
+            if (addNonSupplierChangeToOversight)
+            {
+                parameters.Oversight = Maybe<UpdatedDataProcessingRegistrationOversightDataParameters>.Some(new UpdatedDataProcessingRegistrationOversightDataParameters());
+                parameters.Oversight.Value.OversightCompletedRemark = A<string>().AsChangedValue();
+            }
+            if (addChangeToRoles)
+            {
+                parameters.Roles = Maybe<UpdatedDataProcessingRegistrationRoles>.Some(new UpdatedDataProcessingRegistrationRoles());
+                parameters.Roles.Value.UserRolePairs =
+                    Maybe<IEnumerable<UserRolePair>>.Some(new List<UserRolePair>()).AsChangedValue();
+            }
+            if (addChangeToExternalReferences)
+            {
+                var existingReferences = new List<ExternalReference>() { new ExternalReference(), new ExternalReference() };
+                _existingDpr.ExternalReferences = existingReferences;
+                parameters.ExternalReferences = Maybe<IEnumerable<UpdatedExternalReferenceProperties>>.Some(Many<UpdatedExternalReferenceProperties>());
+            }
+
+            var result = _sut.RequestsChangesToSupplierAssociatedFields(parameters);
+
+            Assert.False(result);
+        }
 
         [Fact]
         public void
