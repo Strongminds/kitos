90193ae4906c8051e4c0047a49af3e6aa845a681
Aleksander Naskret <aln@strongminds.dk>
2025-10-07 09:03:11 +0200

    integration tests

--- Presentation.Web/Controllers/API/V2/External/DataProcessingRegistrations/DataProcessingRegistrationOversightDatesV2Controller.cs
+++ Presentation.Web/Controllers/API/V2/External/DataProcessingRegistrations/DataProcessingRegistrationOversightDatesV2Controller.cs
@@ -1,7 +1,6 @@
 ﻿using Core.ApplicationServices.GDPR.Write;
 using Presentation.Web.Controllers.API.V2.External.DataProcessingRegistrations.Mapping;
 using Presentation.Web.Infrastructure.Attributes;
-using Presentation.Web.Models.API.V2.Request.DataProcessing;
 using Presentation.Web.Models.API.V2.Response.DataProcessing;
 using Swashbuckle.Swagger.Annotations;
 using System;
@@ -15,14 +14,14 @@ namespace Presentation.Web.Controllers.API.V2.External.DataProcessingRegistratio
     /// API for the data processing registrations in KITOS
     /// </summary>
     [RoutePrefix("api/v2/data-processing-registrations/{uuid}/oversight-dates")]
-    public class DataProcessingRegistrationOversightV2Controller : ExternalBaseController
+    public class DataProcessingRegistrationOversightDatesV2Controller : ExternalBaseController
     {
 
         private readonly IDataProcessingRegistrationWriteService _writeService;
         private readonly IDataProcessingRegistrationWriteModelMapper _writeModelMapper;
         private readonly IDataProcessingRegistrationResponseMapper _responseMapper;
 
-        public DataProcessingRegistrationOversightV2Controller(IDataProcessingRegistrationWriteService writeService,
+        public DataProcessingRegistrationOversightDatesV2Controller(IDataProcessingRegistrationWriteService writeService,
             IDataProcessingRegistrationWriteModelMapper writeModelMapper,
             IDataProcessingRegistrationResponseMapper responseMapper)
         {
@@ -39,7 +38,7 @@ namespace Presentation.Web.Controllers.API.V2.External.DataProcessingRegistratio
         /// <returns></returns>
         [HttpPost]
         [Route]
-        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(DataProcessingRegistrationResponseDTO))]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(OversightDateDTO))]
         [SwaggerResponse(HttpStatusCode.BadRequest)]
         [SwaggerResponse(HttpStatusCode.Conflict)]
         [SwaggerResponse(HttpStatusCode.Unauthorized)]
@@ -64,7 +63,7 @@ namespace Presentation.Web.Controllers.API.V2.External.DataProcessingRegistratio
         /// <returns></returns>
         [HttpPatch]
         [Route("{oversightDateUuid}")]
-        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(DataProcessingRegistrationResponseDTO))]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(OversightDateDTO))]
         [SwaggerResponse(HttpStatusCode.BadRequest)]
         [SwaggerResponse(HttpStatusCode.Conflict)]
         [SwaggerResponse(HttpStatusCode.Unauthorized)]
@@ -79,6 +78,30 @@ namespace Presentation.Web.Controllers.API.V2.External.DataProcessingRegistratio
                 .UpdateOversight(uuid, oversightDateUuid, _writeModelMapper.FromOversightPATCH(request))
                 .Select(_responseMapper.MapOversightDate)
                 .Match(Ok, FromOperationError);
+        }
+
+        /// <summary>
+        /// Deletes an oversight date from a data processing registration.
+        /// </summary>
+        /// <param name="uuid">UUID of the data processing registration</param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{oversightDateUuid}")]
+        [SwaggerResponse(HttpStatusCode.NoContent)]
+        [SwaggerResponse(HttpStatusCode.BadRequest)]
+        [SwaggerResponse(HttpStatusCode.Conflict)]
+        [SwaggerResponse(HttpStatusCode.Unauthorized)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        [SwaggerResponse(HttpStatusCode.Forbidden)]
+        public IHttpActionResult DeleteDataProcessingRegistrationOversightDate([NonEmptyGuid] Guid uuid, [NonEmptyGuid] Guid oversightDateUuid, [FromBody] ModifyOversightDateDTO request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return _writeService
+                .DeleteOversight(uuid, oversightDateUuid)
+                .Match(FromOperationError, NoContent);
         }
     }
 }
