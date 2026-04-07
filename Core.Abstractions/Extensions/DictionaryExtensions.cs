cf92f7932af866c266a0d1c0eaf9bb9506756f43
Aleksander Naskret <aln@strongminds.dk>
2026-04-07 12:58:51 +0200

    cleanup

--- Core.Abstractions/Extensions/DictionaryExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Core.Abstractions.Extensions
-{
-    public static class DictionaryExtensions
-    {
-        // AsReadOnly<TKey,TValue> is provided by System.Collections.Generic.CollectionExtensions since .NET 8
-    }
-}
