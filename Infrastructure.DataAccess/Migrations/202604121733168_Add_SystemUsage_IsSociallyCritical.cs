3fbf2bdb691e6e74fd65ab1a720c78d42870a64c
NikolajNielsen <nan@strongminds.dk>
2026-04-12 19:34:27 +0200

    Migrate

---
+++ Infrastructure.DataAccess/Migrations/202604121733168_Add_SystemUsage_IsSociallyCritical.cs
@@ -0,0 +1,20 @@
+﻿namespace Infrastructure.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_SystemUsage_IsSociallyCritical : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ItSystemUsage", "IsSociallyCritical", c => c.Boolean(nullable: false));
+            AddColumn("dbo.ItSystemUsageOverviewReadModels", "IsSociallyCritical", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ItSystemUsageOverviewReadModels", "IsSociallyCritical");
+            DropColumn("dbo.ItSystemUsage", "IsSociallyCritical");
+        }
+    }
+}
