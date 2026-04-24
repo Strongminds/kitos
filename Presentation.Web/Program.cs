ed28b6fbfd10cec686c806d98eea9a80c4ad68d9
Aleksander Naskret <aln@strongminds.dk>
2026-04-24 13:39:42 +0200

    use is Production to check env

--- Presentation.Web/Program.cs
+++ Presentation.Web/Program.cs
@@ -38,7 +38,7 @@ var services = builder.Services;
 services.AddKitosMvc();
 services.AddRouting();
 var signingKey = services.AddKitosAuthentication(configuration);
-services.AddKitosSwagger(builder.Environment.IsDevelopment() || builder.Environment.IsEnvironment("Integration"));
+services.AddKitosSwagger(!builder.Environment.IsProduction());
 services.AddKitosHangfire(configuration);
 
 // AutoMapper - using explicit assembly scanning
