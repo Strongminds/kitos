633cf557a14b3b36b1a4df9b790adf6bb611ee35
NikolajNielsen <nan@strongminds.dk>
2026-04-28 13:13:48 +0200

    Migrate name

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260428111316_UpdateIsBusinessCriticalPropertyName.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class UpdateIsBusinessCriticalPropertyName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItSystemUsage_IsSociallyCritical",
+                table: "ItSystemUsage");
+
+            migrationBuilder.DropColumn(
+                name: "ItSystemUsage_isBusinessCritical",
+                table: "ItSystemUsage");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ItSystemUsage_IsSociallyCritical",
+                table: "ItSystemUsage",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ItSystemUsage_isBusinessCritical",
+                table: "ItSystemUsage",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
