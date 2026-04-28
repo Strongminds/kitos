fbb7271da58ebfe9d6aeec2824eb01d1e17b486a
NikolajNielsen <nan@strongminds.dk>
2026-04-28 09:47:56 +0200

    Migration for lastChanged

--- Infrastructure.DataAccess/Migrations/EfCore/20260428074600_AddSystemCriticalityLastChanged.Designer.cs
+++ Infrastructure.DataAccess/Migrations/EfCore/20260428074600_AddSystemCriticalityLastChanged.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.DataAccess.Migrations.EfCore
 {
     [DbContext(typeof(KitosContext))]
-    partial class KitosContextModelSnapshot : ModelSnapshot
+    [Migration("20260428074600_AddSystemCriticalityLastChanged")]
+    partial class AddSystemCriticalityLastChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260428074600_AddSystemCriticalityLastChanged.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class AddSystemCriticalityLastChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CriticalityInfo_LastChanged",
+                table: "ItSystemUsage",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ItSystemUsage_IsSociallyCritical",
+                table: "ItSystemUsage",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ItSystemUsage_isBusinessCritical",
+                table: "ItSystemUsage",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CriticalityInfo_LastChanged",
+                table: "ItSystemUsage");
+
+            migrationBuilder.DropColumn(
+                name: "ItSystemUsage_IsSociallyCritical",
+                table: "ItSystemUsage");
+
+            migrationBuilder.DropColumn(
+                name: "ItSystemUsage_isBusinessCritical",
+                table: "ItSystemUsage");
+        }
+    }
+}
