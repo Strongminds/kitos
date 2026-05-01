8829d0da05c696c90c4c8518ada9ea2dd1f8cae8
NikolajNielsen <nan@strongminds.dk>
2026-05-01 12:35:31 +0200

    Migration

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260501103523_AddCriticalityLevelName_ToUsageReadModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class AddCriticalityLevelName_ToUsageReadModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CriticalityLevelDocumentationUrlName",
+                table: "ItSystemUsageOverviewReadModels",
+                newName: "SystemUsageCriticalityLevelName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CriticalityLevelDocumentationName",
+                table: "ItSystemUsageOverviewReadModels",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CriticalityLevelDocumentationName",
+                table: "ItSystemUsageOverviewReadModels");
+
+            migrationBuilder.RenameColumn(
+                name: "SystemUsageCriticalityLevelName",
+                table: "ItSystemUsageOverviewReadModels",
+                newName: "CriticalityLevelDocumentationUrlName");
+        }
+    }
+}
