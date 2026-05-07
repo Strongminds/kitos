3d81072a1d685e61c80196554298455a890f8107
Aleksander Naskret <aln@strongminds.dk>
2026-05-07 10:34:16 +0200

    Manually push app_offline file

--- DeploymentScripts/DeployWebsite.ps1
+++ DeploymentScripts/DeployWebsite.ps1
@@ -1,6 +1,19 @@
 Function Deploy-Website($packageDirectory, $msDeployUrl, $msDeployUser, $msDeployPassword) {
 
     $msdeploy = "C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe"
+    $destArgs = "computerName=`"{0}`",userName=`"{1}`",password=`"{2}`",authtype=`"Basic`",includeAcls=`"False`"" `
+        -f $msDeployUrl, $msDeployUser, $msDeployPassword
+
+    # AppOfflineRule is unavailable in some Web Deploy installations.
+    # Instead, manually push app_offline.htm before syncing and delete it after.
+    $appOfflineTmp = [System.IO.Path]::GetTempFileName()
+    Copy-Item "$PSScriptRoot\..\App_Offline.html" $appOfflineTmp -Force
+
+    $putOffline = ("`"{0}`" -verb:sync -source:contentPath=`"{1}`" " +
+                   "-dest:contentPath=`"Default Web Site/app_offline.htm`",{2} -allowUntrusted") `
+        -f $msdeploy, $appOfflineTmp, $destArgs
+    & cmd.exe /C $putOffline
+    if($LASTEXITCODE -ne 0) { throw "FAILED TO PUT SITE OFFLINE" }
 
     # All environment-specific configuration is now baked into appsettings.json by Prepare-Package.
     # This function only handles the file sync to the remote IIS server.
@@ -8,8 +21,7 @@ Function Deploy-Website($packageDirectory, $msDeployUrl, $msDeployUser, $msDeplo
     $fullCommand = ("`"{0}`" " +
                     "-verb:sync " +
                     "-source:package=`"{1}\Presentation.Web.zip`" " +
-                    "-dest:contentPath=`"Default Web Site`",computerName=`"{2}`",userName=`"{3}`",password=`"{4}`",authtype=`"Basic`",includeAcls=`"False`" " +
-                    "-enableRule:AppOfflineRule " +
+                    "-dest:contentPath=`"Default Web Site`",{2} " +
                     "-disableLink:AppPoolExtension " +
                     "-disableLink:ContentExtension " +
                     "-disableLink:CertificateExtension " +
@@ -17,9 +29,18 @@ Function Deploy-Website($packageDirectory, $msDeployUrl, $msDeployUser, $msDeplo
                     "-skip:objectname=`"dirPath`",absolutepath=`".*\\runtimes($|\\.*)`" " +
                     "-skip:objectname=`"filePath`",absolutepath=`".*\\runtimes\\.*`" " +
                     "-allowUntrusted") `
-    -f $msdeploy, $packageDirectory, $msDeployUrl, $msDeployUser, $msDeployPassword
+    -f $msdeploy, $packageDirectory, $destArgs
 
     & cmd.exe /C $fullCommand
+    $deployExitCode = $LASTEXITCODE
 
-    if($LASTEXITCODE -ne 0) { throw "FAILED TO DEPLOY" }
+    # Always bring the site back online by removing app_offline.htm
+    $deleteOffline = ("`"{0}`" -verb:delete " +
+                      "-dest:contentPath=`"Default Web Site/app_offline.htm`",{1} -allowUntrusted") `
+        -f $msdeploy, $destArgs
+    & cmd.exe /C $deleteOffline
+
+    Remove-Item $appOfflineTmp -Force -ErrorAction SilentlyContinue
+
+    if($deployExitCode -ne 0) { throw "FAILED TO DEPLOY" }
 }
