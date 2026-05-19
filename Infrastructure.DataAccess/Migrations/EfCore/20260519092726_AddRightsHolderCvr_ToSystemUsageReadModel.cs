ef421a282a20c6e42f0d36775cc208d00bed412b
NikolajNielsen <nan@strongminds.dk>
2026-05-19 11:27:38 +0200

    Migration

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260519092726_AddRightsHolderCvr_ToSystemUsageReadModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class AddRightsHolderCvr_ToSystemUsageReadModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ItSystemRightsHolderCvr",
+                table: "ItSystemUsageOverviewReadModels",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItSystemRightsHolderCvr",
+                table: "ItSystemUsageOverviewReadModels");
+        }
+    }
+}
