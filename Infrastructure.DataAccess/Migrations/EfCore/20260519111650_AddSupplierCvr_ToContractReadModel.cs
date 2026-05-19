95436b48404a75a170e6cc59c320fd868764225f
NikolajNielsen <nan@strongminds.dk>
2026-05-19 13:16:55 +0200

    Contract migration

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260519111650_AddSupplierCvr_ToContractReadModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class AddSupplierCvr_ToContractReadModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SupplierCvr",
+                table: "ItContractOverviewReadModels",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SupplierCvr",
+                table: "ItContractOverviewReadModels");
+        }
+    }
+}
