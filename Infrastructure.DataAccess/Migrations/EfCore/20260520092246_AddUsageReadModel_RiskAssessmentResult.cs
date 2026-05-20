113397568fbc6d20f896bd1a90ebf6b8c8a1ed6c
NikolajNielsen <nan@strongminds.dk>
2026-05-20 11:34:33 +0200

    Migration for risk assessment result

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260520092246_AddUsageReadModel_RiskAssessmentResult.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class AddUsageReadModel_RiskAssessmentResult : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RiskAssessmentResult",
+                table: "ItSystemUsageOverviewReadModels",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RiskAssessmentResult",
+                table: "ItSystemUsageOverviewReadModels");
+        }
+    }
+}
