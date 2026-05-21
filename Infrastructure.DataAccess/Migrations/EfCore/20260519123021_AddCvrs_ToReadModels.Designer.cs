ebc13c406a1f3d0e52e173e9e880d4d0a3aa2d66
Nikolaj Nielsen <121798106+strongmindsnan@users.noreply.github.com>
2026-05-21 08:36:08 +0200

    6071 rightsholder cvr in grid (#1408)
    * .
    
    * Add cvr to usage readmodel
    
    * Rename
    
    * Update api test
    
    * Migration
    
    * Add supplier cvr to contract readmodel
    
    * Contract migration
    
    * Migration of cvrs
    
    * Fix api test
    
    * Add cvr field for dpr data processors
    
    * Migration for dpr
    
    * Use A()

--- Infrastructure.DataAccess/Migrations/EfCore/20260519123021_AddCvrs_ToReadModels.Designer.cs
+++ Infrastructure.DataAccess/Migrations/EfCore/20260519123021_AddCvrs_ToReadModels.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.DataAccess.Migrations.EfCore
 {
     [DbContext(typeof(KitosContext))]
-    partial class KitosContextModelSnapshot : ModelSnapshot
+    [Migration("20260519123021_AddCvrs_ToReadModels")]
+    partial class AddCvrs_ToReadModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -895,9 +898,6 @@ namespace Infrastructure.DataAccess.Migrations.EfCore
                     b.Property<string>("ContractNamesAsCsv")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("DataProcessorCvrsAsCsv")
-                        .HasColumnType("nvarchar(max)");
-
                     b.Property<string>("DataProcessorNamesAsCsv")
                         .HasColumnType("nvarchar(max)");
 
@@ -980,9 +980,6 @@ namespace Infrastructure.DataAccess.Migrations.EfCore
 
                     b.Property<Guid>("SourceEntityUuid")
                         .HasColumnType("uniqueidentifier");
-
-                    b.Property<string>("SubDataProcessorCvrsAsCsv")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("SubDataProcessorNamesAsCsv")
                         .HasColumnType("nvarchar(max)");
