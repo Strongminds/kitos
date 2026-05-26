a99b774126975fa10c1b3d6b34e2b720413928c1
NikolajNielsen <nan@strongminds.dk>
2026-05-26 10:30:42 +0200

    Remove explicit bit type in migration for postgres compatibility

--- Infrastructure.DataAccess/Migrations/EfCore/20260523084814_UpdateDPR_AddEnforceInvalidityField.cs
+++ Infrastructure.DataAccess/Migrations/EfCore/20260523084814_UpdateDPR_AddEnforceInvalidityField.cs
@@ -13,7 +13,6 @@ namespace Infrastructure.DataAccess.Migrations.EfCore
             migrationBuilder.AddColumn<bool>(
                 name: "EnforceInvalidity",
                 table: "DataProcessingRegistrations",
-                type: "bit",
                 nullable: true);
         }
 
