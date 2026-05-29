7124b0c9c61665a24d7aaac5f543390a26e695f9
NikolajNielsen <nan@strongminds.dk>
2026-05-29 19:40:19 +0200

    Use required keyword

--- Presentation.Web/Models/API/V2/Response/DataProcessing/DataProcessingRegistrationResponseDTO.cs
+++ Presentation.Web/Models/API/V2/Response/DataProcessing/DataProcessingRegistrationResponseDTO.cs
@@ -12,38 +12,27 @@ namespace Presentation.Web.Models.API.V2.Response.DataProcessing
 {
     public class DataProcessingRegistrationResponseDTO : IHasNameExternal, IHasUuidExternal, IHasEntityCreator, IHasLastModified, IHasOrganizationContext
     {
-        [Required]
-        public string Name { get; set; }
-        [Required]
-        public Guid Uuid { get; set; }
-        [Required]
-        public IdentityNamePairResponseDTO CreatedBy { get; set; }
+        public required string Name { get; set; }
+        public required Guid Uuid { get; set; }
+        public required IdentityNamePairResponseDTO CreatedBy { get; set; }
         /// <summary>
         /// UTC timestamp of latest modification
         /// </summary>
-        [Required]
-        public DateTime LastModified { get; set; }
-        [Required]
-        public IdentityNamePairResponseDTO LastModifiedBy { get; set; }
-        [Required]
-        public ShallowOrganizationResponseDTO OrganizationContext { get; set; }
-        [Required]
-        public DataProcessingRegistrationGeneralDataResponseDTO General { get; set; }
+        public required DateTime LastModified { get; set; }
+        public required IdentityNamePairResponseDTO LastModifiedBy { get; set; }
+        public required ShallowOrganizationResponseDTO OrganizationContext { get; set; }
+        public required DataProcessingRegistrationGeneralDataResponseDTO General { get; set; }
         /// <summary>
         /// Associated it-system-usage entities
-        [Required]
-        public IEnumerable<ShallowItSystemUsageResponseDTO> SystemUsages { get; set; }
-        [Required]
-        public DataProcessingRegistrationOversightResponseDTO Oversight { get; set; }
+        public required IEnumerable<ShallowItSystemUsageResponseDTO> SystemUsages { get; set; }
+        public required DataProcessingRegistrationOversightResponseDTO Oversight { get; set; }
         /// <summary>
         /// Data processing role assignments
         /// </summary>
-        [Required]
-        public IEnumerable<RoleAssignmentResponseDTO> Roles { get; set; }
+        public required IEnumerable<RoleAssignmentResponseDTO> Roles { get; set; }
         /// <summary>
         /// External reference definitions
         /// </summary>
-        [Required]
-        public IEnumerable<ExternalReferenceDataResponseDTO> ExternalReferences { get; set; }
+        public required IEnumerable<ExternalReferenceDataResponseDTO> ExternalReferences { get; set; }
     }
 }
