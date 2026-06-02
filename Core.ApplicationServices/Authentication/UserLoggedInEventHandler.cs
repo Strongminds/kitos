89658d50f655b4962a0add6416d30f5f0090cba1
NikolajNielsen <nan@strongminds.dk>
2026-06-02 10:42:34 +0200

    Handle error in handler

--- Core.ApplicationServices/Authentication/UserLoggedInEventHandler.cs
+++ Core.ApplicationServices/Authentication/UserLoggedInEventHandler.cs
@@ -1,16 +1,20 @@
 ﻿using Core.ApplicationServices.Users;
 using Core.DomainModel.Events;
+using Serilog;
 using System;
 
 namespace Core.ApplicationServices.Authentication
 {
-    public class UserLoggedInEventHandler(IUserLoggedInService userLoggedInService) : IDomainEventHandler<UserLoggedInEvent>
+    public class UserLoggedInEventHandler(IUserLoggedInService userLoggedInService, ILogger logger) : IDomainEventHandler<UserLoggedInEvent>
     {
         public void Handle(UserLoggedInEvent domainEvent)
         {
             var uuid = domainEvent.UserUuid;
             var now = DateTime.UtcNow;
-            userLoggedInService.UpdateLatestLoginWithoutAuthenticating(uuid, now);
+            
+            var errorMaybe = userLoggedInService.UpdateLatestLoginWithoutAuthenticating(uuid, now);
+
+            if (errorMaybe.HasValue) logger.Error($"Error handling user login event: {errorMaybe.Value.Message}");
         }
     }
 }
