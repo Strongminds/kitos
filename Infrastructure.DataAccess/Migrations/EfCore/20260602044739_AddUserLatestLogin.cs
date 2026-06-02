f393e8410be013465a03a1911169ebe46662930f
NikolajNielsen <nan@strongminds.dk>
2026-06-02 07:11:54 +0200

    Migration

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260602044739_AddUserLatestLogin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class AddUserLatestLogin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LatestLogin",
+                table: "User",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LatestLogin",
+                table: "User");
+        }
+    }
+}
