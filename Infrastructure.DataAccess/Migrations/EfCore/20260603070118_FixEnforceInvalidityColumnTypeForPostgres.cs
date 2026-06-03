1c8283bdcb10b0c7619c476e91ef78d85eacf35f
Aleksander Naskret <aln@strongminds.dk>
2026-06-03 09:23:18 +0200

    fix type

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260603070118_FixEnforceInvalidityColumnTypeForPostgres.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class FixEnforceInvalidityColumnTypeForPostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "EnforceInvalidity",
+                table: "DataProcessingRegistrations",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "EnforceInvalidity",
+                table: "DataProcessingRegistrations",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+        }
+    }
+}
