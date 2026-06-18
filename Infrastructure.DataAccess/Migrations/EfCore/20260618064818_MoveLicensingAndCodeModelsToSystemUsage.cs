0d5652b35dddaf15845c13c7ee49ff408224df70
NikolajNielsen <nan@strongminds.dk>
2026-06-18 08:48:26 +0200

    Migration for moving licensing and code models to system usage

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260618064818_MoveLicensingAndCodeModelsToSystemUsage.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class MoveLicensingAndCodeModelsToSystemUsage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LicensingAndCodeModels",
+                table: "ItSystem");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LicensingAndCodeModels",
+                table: "ItSystemUsage",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LicensingAndCodeModels",
+                table: "ItSystemUsage");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LicensingAndCodeModels",
+                table: "ItSystem",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
