0ca198cf750b6ed925f2dfa7a82dff9477e79e3c
NikolajNielsen <nan@strongminds.dk>
2026-06-19 11:00:27 +0200

    Copilot add in/out dtos

--- Presentation.Web/Controllers/API/V2/External/ItSystemUsages/ItSystemUsageArchivesV2Controller.cs
+++ Presentation.Web/Controllers/API/V2/External/ItSystemUsages/ItSystemUsageArchivesV2Controller.cs
@@ -1,20 +1,35 @@
 ﻿using Core.ApplicationServices.Model.SystemUsage;
 using Microsoft.AspNetCore.Mvc;
+using Presentation.Web.Controllers.API.V2.External.ItSystemUsages.Mapping;
 using Presentation.Web.Infrastructure.Attributes;
+using Presentation.Web.Models.API.V2.Request.SystemUsage;
 using System;
 
 namespace Presentation.Web.Controllers.API.V2.External.ItSystemUsages
 {
     [Route("api/v2/it-system-usages/archives")]
-    public class ItSystemUsageArchivesV2Controller(IItSystemUsageArchiveService archivedItSystemUsageService) : ExternalBaseController
+    public class ItSystemUsageArchivesV2Controller(
+        IItSystemUsageArchiveService archivedItSystemUsageService,
+        IItSystemArchiveResponseMapper responseMapper) : ExternalBaseController
     {
         [HttpPost]
         [Route("{systemUsageUuid}")]
-        public IActionResult ArchiveItsystemUsage([NonEmptyGuid] Guid systemUsageUuid)
+        public IActionResult ArchiveItsystemUsage(
+            [NonEmptyGuid] Guid systemUsageUuid,
+            [FromBody] CreateItSystemUsageArchiveRequestDTO request)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            return archivedItSystemUsageService.Create(systemUsageUuid).Match(Ok, FromOperationError);
+            var parameters = new CreateItSystemArchiveParameters
+            {
+                ArchivingDate = request.ArchivingDate,
+                ReferenceName = request.ReferenceName,
+                Note = request.Note
+            };
+
+            return archivedItSystemUsageService.Create(systemUsageUuid, parameters).Match(
+                archive => Ok(responseMapper.ToResponseDTO(archive)),
+                FromOperationError);
         }
     }
 }
