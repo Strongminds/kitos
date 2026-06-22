4c2ca7581b01e71b284b166003662f35fed9535f
Aleksander Naskret <aln@strongminds.dk>
2026-06-22 10:06:53 +0200

    move archivereference properties to correct folder

--- Core.ApplicationServices/Model/SystemUsage/ArchiveReferenceProperties.cs
+++
@@ -1,8 +0,0 @@
-namespace Core.ApplicationServices.Model.SystemUsage
-{
-    public class ArchiveReferenceProperties
-    {
-        public required string Label { get; set; }
-        public required string Url { get; set; }
-    }
-}
