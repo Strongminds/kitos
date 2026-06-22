4b2c9dbfb551a0113d0a9c5986aa3e391d6edc31
Aleksander Naskret <aln@strongminds.dk>
2026-06-22 10:36:49 +0200

    fix migration

--- Infrastructure.DataAccess/Migrations/EfCore/20260622074604_IntroduceArchivingEntities.cs
+++ Infrastructure.DataAccess/Migrations/EfCore/20260622074604_IntroduceArchivingEntities.cs
@@ -11,68 +11,30 @@ namespace Infrastructure.DataAccess.Migrations.EfCore
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_ItSystemUsage_TechnicalSystemTypes_TechnicalSystemTypeId",
-                table: "ItSystemUsage");
-
-            migrationBuilder.DropIndex(
-                name: "ItSystemUsageOverviewReadModel_Index_TechnicalSystemTypeUuid",
-                table: "ItSystemUsageOverviewReadModels");
-
-            migrationBuilder.DropIndex(
-                name: "IX_ItSystemUsage_TechnicalSystemTypeId",
-                table: "ItSystemUsage");
-
-            migrationBuilder.DropColumn(
-                name: "TechnicalSystemTypeUuid",
-                table: "ItSystemUsageOverviewReadModels");
-
-            migrationBuilder.DropColumn(
-                name: "TechnicalSystemTypeId",
-                table: "ItSystemUsage");
-
-            migrationBuilder.RenameColumn(
-                name: "TechnicalSystemTypeName",
-                table: "ItSystemUsageOverviewReadModels",
-                newName: "TechnicalSystemTypeNamesAsCsv");
-
-            migrationBuilder.AddColumn<string>(
-                name: "ItInterfaceIdsAsCsv",
-                table: "ItSystemUsageOverviewReadModels",
-                type: "nvarchar(max)",
-                nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "ItInterfaceVersionsAsCsv",
-                table: "ItSystemUsageOverviewReadModels",
-                type: "nvarchar(max)",
-                nullable: true);
-
-            migrationBuilder.AddUniqueConstraint(
-                name: "AK_Organization_Uuid",
-                table: "Organization",
-                column: "Uuid");
-
-            migrationBuilder.AddUniqueConstraint(
-                name: "AK_ItSystem_Uuid",
-                table: "ItSystem",
-                column: "Uuid");
+            var isSqlServer = migrationBuilder.ActiveProvider == "Microsoft.EntityFrameworkCore.SqlServer";
+            var maxTextType = isSqlServer ? "nvarchar(max)" : "text";
+            var uuidType = isSqlServer ? "uniqueidentifier" : "uuid";
+            var intType = isSqlServer ? "int" : "integer";
+            var dateTimeType = isSqlServer ? "datetime2" : "timestamp without time zone";
 
             migrationBuilder.CreateTable(
                 name: "ItSystemArchive",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Uuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    SnapshotUuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    OrganizationUuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Note = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    ArchivingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    ReferenceName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    ObjectOwnerId = table.Column<int>(type: "int", nullable: false),
-                    LastChanged = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    LastChangedByUserId = table.Column<int>(type: "int", nullable: false)
+                    Id = isSqlServer
+                        ? table.Column<int>(type: intType, nullable: false)
+                            .Annotation("SqlServer:Identity", "1, 1")
+                        : table.Column<int>(type: intType, nullable: false)
+                            .Annotation("Npgsql:ValueGenerationStrategy", Npgsql.EntityFrameworkCore.PostgreSQL.Metadata.NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Uuid = table.Column<Guid>(type: uuidType, nullable: false),
+                    SnapshotUuid = table.Column<Guid>(type: uuidType, nullable: false),
+                    OrganizationUuid = table.Column<Guid>(type: uuidType, nullable: false),
+                    Note = table.Column<string>(type: maxTextType, nullable: false),
+                    ArchivingDate = table.Column<DateTime>(type: dateTimeType, nullable: false),
+                    ReferenceName = table.Column<string>(type: maxTextType, nullable: false),
+                    ObjectOwnerId = table.Column<int>(type: intType, nullable: false),
+                    LastChanged = table.Column<DateTime>(type: dateTimeType, nullable: false),
+                    LastChangedByUserId = table.Column<int>(type: intType, nullable: false)
                 },
                 constraints: table =>
                 {
@@ -99,63 +61,21 @@ namespace Infrastructure.DataAccess.Migrations.EfCore
                 });
 
             migrationBuilder.CreateTable(
-                name: "ItSystemUsageOverviewTechnicalSystemTypeReadModel",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    TechnicalSystemTypeUuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    TechnicalSystemTypeName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
-                    ParentId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ItSystemUsageOverviewTechnicalSystemTypeReadModel", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_ItSystemUsageOverviewTechnicalSystemTypeReadModel_ItSystemUsageOverviewReadModels_ParentId",
-                        column: x => x.ParentId,
-                        principalTable: "ItSystemUsageOverviewReadModels",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "ItSystemUsageTechnicalSystemTypes",
-                columns: table => new
-                {
-                    ReferencesId = table.Column<int>(type: "int", nullable: false),
-                    TechnicalSystemTypesId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ItSystemUsageTechnicalSystemTypes", x => new { x.ReferencesId, x.TechnicalSystemTypesId });
-                    table.ForeignKey(
-                        name: "FK_ItSystemUsageTechnicalSystemTypes_ItSystemUsage_ReferencesId",
-                        column: x => x.ReferencesId,
-                        principalTable: "ItSystemUsage",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_ItSystemUsageTechnicalSystemTypes_TechnicalSystemTypes_TechnicalSystemTypesId",
-                        column: x => x.TechnicalSystemTypesId,
-                        principalTable: "TechnicalSystemTypes",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
                 name: "ArchiveReference",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Uuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Label = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    ItSystemArchiveUuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    ObjectOwnerId = table.Column<int>(type: "int", nullable: false),
-                    LastChanged = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    LastChangedByUserId = table.Column<int>(type: "int", nullable: false)
+                    Id = isSqlServer
+                        ? table.Column<int>(type: intType, nullable: false)
+                            .Annotation("SqlServer:Identity", "1, 1")
+                        : table.Column<int>(type: intType, nullable: false)
+                            .Annotation("Npgsql:ValueGenerationStrategy", Npgsql.EntityFrameworkCore.PostgreSQL.Metadata.NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Uuid = table.Column<Guid>(type: uuidType, nullable: false),
+                    Label = table.Column<string>(type: maxTextType, nullable: false),
+                    Url = table.Column<string>(type: maxTextType, nullable: false),
+                    ItSystemArchiveUuid = table.Column<Guid>(type: uuidType, nullable: false),
+                    ObjectOwnerId = table.Column<int>(type: intType, nullable: false),
+                    LastChanged = table.Column<DateTime>(type: dateTimeType, nullable: false),
+                    LastChangedByUserId = table.Column<int>(type: intType, nullable: false)
                 },
                 constraints: table =>
                 {
@@ -184,17 +104,20 @@ namespace Infrastructure.DataAccess.Migrations.EfCore
                 name: "Snapshot",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Uuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    LegacyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LocalName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LocalId = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    ItSystemUuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    ItSystemArchiveUuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    ObjectOwnerId = table.Column<int>(type: "int", nullable: false),
-                    LastChanged = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    LastChangedByUserId = table.Column<int>(type: "int", nullable: false)
+                    Id = isSqlServer
+                        ? table.Column<int>(type: intType, nullable: false)
+                            .Annotation("SqlServer:Identity", "1, 1")
+                        : table.Column<int>(type: intType, nullable: false)
+                            .Annotation("Npgsql:ValueGenerationStrategy", Npgsql.EntityFrameworkCore.PostgreSQL.Metadata.NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Uuid = table.Column<Guid>(type: uuidType, nullable: false),
+                    LegacyName = table.Column<string>(type: maxTextType, nullable: true),
+                    LocalName = table.Column<string>(type: maxTextType, nullable: true),
+                    LocalId = table.Column<string>(type: maxTextType, nullable: true),
+                    ItSystemUuid = table.Column<Guid>(type: uuidType, nullable: false),
+                    ItSystemArchiveUuid = table.Column<Guid>(type: uuidType, nullable: false),
+                    ObjectOwnerId = table.Column<int>(type: intType, nullable: false),
+                    LastChanged = table.Column<DateTime>(type: dateTimeType, nullable: false),
+                    LastChangedByUserId = table.Column<int>(type: intType, nullable: false)
                 },
                 constraints: table =>
                 {
@@ -224,11 +147,6 @@ namespace Infrastructure.DataAccess.Migrations.EfCore
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Restrict);
                 });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Category_Id",
-                table: "PendingReadModelUpdates",
-                columns: new[] { "Category", "Id" });
 
             migrationBuilder.CreateIndex(
                 name: "IX_ArchiveReference_ItSystemArchiveUuid",
@@ -279,21 +197,6 @@ namespace Infrastructure.DataAccess.Migrations.EfCore
                 unique: true);
 
             migrationBuilder.CreateIndex(
-                name: "ItSystemUsageOverviewTechnicalSystemTypeReadModel_Index_Uuid",
-                table: "ItSystemUsageOverviewTechnicalSystemTypeReadModel",
-                column: "TechnicalSystemTypeUuid");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ItSystemUsageOverviewTechnicalSystemTypeReadModel_ParentId",
-                table: "ItSystemUsageOverviewTechnicalSystemTypeReadModel",
-                column: "ParentId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ItSystemUsageTechnicalSystemTypes_TechnicalSystemTypesId",
-                table: "ItSystemUsageTechnicalSystemTypes",
-                column: "TechnicalSystemTypesId");
-
-            migrationBuilder.CreateIndex(
                 name: "IX_ItSystemUsageArchiveSnapshot_ItSystemUuid",
                 table: "Snapshot",
                 column: "ItSystemUuid");
@@ -328,70 +231,10 @@ namespace Infrastructure.DataAccess.Migrations.EfCore
                 name: "ArchiveReference");
 
             migrationBuilder.DropTable(
-                name: "ItSystemUsageOverviewTechnicalSystemTypeReadModel");
-
-            migrationBuilder.DropTable(
-                name: "ItSystemUsageTechnicalSystemTypes");
-
-            migrationBuilder.DropTable(
                 name: "Snapshot");
 
             migrationBuilder.DropTable(
                 name: "ItSystemArchive");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Category_Id",
-                table: "PendingReadModelUpdates");
-
-            migrationBuilder.DropUniqueConstraint(
-                name: "AK_Organization_Uuid",
-                table: "Organization");
-
-            migrationBuilder.DropUniqueConstraint(
-                name: "AK_ItSystem_Uuid",
-                table: "ItSystem");
-
-            migrationBuilder.DropColumn(
-                name: "ItInterfaceIdsAsCsv",
-                table: "ItSystemUsageOverviewReadModels");
-
-            migrationBuilder.DropColumn(
-                name: "ItInterfaceVersionsAsCsv",
-                table: "ItSystemUsageOverviewReadModels");
-
-            migrationBuilder.RenameColumn(
-                name: "TechnicalSystemTypeNamesAsCsv",
-                table: "ItSystemUsageOverviewReadModels",
-                newName: "TechnicalSystemTypeName");
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "TechnicalSystemTypeUuid",
-                table: "ItSystemUsageOverviewReadModels",
-                type: "uniqueidentifier",
-                nullable: true);
-
-            migrationBuilder.AddColumn<int>(
-                name: "TechnicalSystemTypeId",
-                table: "ItSystemUsage",
-                type: "int",
-                nullable: true);
-
-            migrationBuilder.CreateIndex(
-                name: "ItSystemUsageOverviewReadModel_Index_TechnicalSystemTypeUuid",
-                table: "ItSystemUsageOverviewReadModels",
-                column: "TechnicalSystemTypeUuid");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ItSystemUsage_TechnicalSystemTypeId",
-                table: "ItSystemUsage",
-                column: "TechnicalSystemTypeId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_ItSystemUsage_TechnicalSystemTypes_TechnicalSystemTypeId",
-                table: "ItSystemUsage",
-                column: "TechnicalSystemTypeId",
-                principalTable: "TechnicalSystemTypes",
-                principalColumn: "Id");
         }
     }
 }
