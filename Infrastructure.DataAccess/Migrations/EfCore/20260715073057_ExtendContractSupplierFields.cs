e163ddb76a2aa2c24afbc4487d0b24073e1f9311
Aleksander Naskret <aln@strongminds.dk>
2026-07-15 09:31:46 +0200

    supplier fields migration

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260715073057_ExtendContractSupplierFields.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class ExtendContractSupplierFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SupplierContactEmail",
+                table: "ItContract",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupplierContactPerson",
+                table: "ItContract",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupplierContactPhoneNumber",
+                table: "ItContract",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SupplierOrganizationUnitId",
+                table: "ItContract",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UseSupplierContractSignerAsContactPerson",
+                table: "ItContract",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItContract_SupplierOrganizationUnitId",
+                table: "ItContract",
+                column: "SupplierOrganizationUnitId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItContract_OrganizationUnit_SupplierOrganizationUnitId",
+                table: "ItContract",
+                column: "SupplierOrganizationUnitId",
+                principalTable: "OrganizationUnit",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItContract_OrganizationUnit_SupplierOrganizationUnitId",
+                table: "ItContract");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ItContract_SupplierOrganizationUnitId",
+                table: "ItContract");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierContactEmail",
+                table: "ItContract");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierContactPerson",
+                table: "ItContract");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierContactPhoneNumber",
+                table: "ItContract");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierOrganizationUnitId",
+                table: "ItContract");
+
+            migrationBuilder.DropColumn(
+                name: "UseSupplierContractSignerAsContactPerson",
+                table: "ItContract");
+        }
+    }
+}
