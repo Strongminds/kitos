5a4b3ab98df00e55c155ea2320863dfb28ac61f4
Aleksander Naskret <aln@strongminds.dk>
2026-08-06 10:16:16 +0200

    recommended field migration

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260806081422_AddRecommendedFieldToUiNode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class AddRecommendedFieldToUiNode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Recommended",
+                table: "CustomizedUiNodes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Recommended",
+                table: "CustomizedUiNodes");
+        }
+    }
+}
