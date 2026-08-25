98fdb963e6cd4857cd7dbf66ef599ac67bbcb8b4
Nikolaj Nielsen <121798106+strongmindsnan@users.noreply.github.com>
2026-08-25 11:11:51 +0200

    6256 models for local field configuration (#1500)
    * Add domain model and connect to organization
    
    * Align with EF expectations
    
    * Migration
    
    * Fix namespace
    
    * Add EF map
    
    * New migration

---
+++ Core.DomainModel/SupplierAssociatedFields/SupplierAssociatedFieldConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.DomainModel.SupplierAssociatedFields
+{
+    public class SupplierAssociatedFieldConfiguration: Entity, IOwnedByOrganization
+    {
+        public string FieldKey { get; set; }
+        public SupplierAssociatedFieldControlState ControlState { get; set; }
+        public int OrganizationId { get; set; }
+        public virtual Organization.Organization Organization { get; set; }
+    }
+}
---
+++ Core.DomainModel/SupplierAssociatedFields/SupplierAssociatedFieldControlState.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.DomainModel.SupplierAssociatedFields
+{
+    public enum SupplierAssociatedFieldControlState
+    {
+        ORGANIZATION = 0,
+        SUPPLIER = 1,
+        SHARED = 2,
+    }
+}
