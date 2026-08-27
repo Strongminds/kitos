c5262429c7e06c5110b2e7aa9a1ccc4ca3cd82e2
Aleksander Naskret <aln@strongminds.dk>
2026-08-27 10:47:32 +0200

    possible null reference cleanup

--- Core.DomainServices/Queries/Organization/QueryByCvrContent.cs
+++ Core.DomainServices/Queries/Organization/QueryByCvrContent.cs
@@ -5,17 +5,18 @@ namespace Core.DomainServices.Queries.Organization
 {
     public class QueryByCvrContent : IDomainQuery<DomainModel.Organization.Organization>
     {
-        private readonly string _cvrNumberContent;
-        private readonly string _cvrNumberContentLower;
+        private readonly string? _cvrNumberContentLower;
 
-        public QueryByCvrContent(string cvrNumberContent)
+        public QueryByCvrContent(string? cvrNumberContent)
         {
-            _cvrNumberContent = cvrNumberContent;
             _cvrNumberContentLower = cvrNumberContent?.ToLower();
         }
 
         public IQueryable<DomainModel.Organization.Organization> Apply(IQueryable<DomainModel.Organization.Organization> source)
         {
+            if(string.IsNullOrEmpty(_cvrNumberContentLower))
+                return source;
+
             return source.Where(x => x.Cvr != null && x.Cvr.ToLower().Contains(_cvrNumberContentLower));
         }
     }
