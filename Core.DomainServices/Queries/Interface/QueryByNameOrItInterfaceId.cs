6c6bfd4500d66b615d290b93152ea1562c51854e
Aleksander Naskret <aln@strongminds.dk>
2026-08-27 10:28:06 +0200

    possible null reference fix

--- Core.DomainServices/Queries/Interface/QueryByNameOrItInterfaceId.cs
+++ Core.DomainServices/Queries/Interface/QueryByNameOrItInterfaceId.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Core.DomainModel.ItSystem;
 
 namespace Core.DomainServices.Queries.Interface
 {
     public class QueryByNameOrItInterfaceId: IDomainQuery<ItInterface>
     {
-        private readonly string _query;
-        private readonly string _queryLower;
+        private readonly string? _queryLower;
 
-        public QueryByNameOrItInterfaceId(string query)
+        public QueryByNameOrItInterfaceId(string? query)
         {
-            _query = query;
             _queryLower = query?.ToLower();
         }
 
         public IQueryable<ItInterface> Apply(IQueryable<ItInterface> source)
         {
+            if (_queryLower == null) return source;
             return source.Where(x => x.Name.ToLower().Contains(_queryLower) || x.ItInterfaceId.ToLower().Contains(_queryLower));
         }
     }
--- Core.DomainServices/Queries/Organization/QueryByNameOrCvrContent.cs
+++ Core.DomainServices/Queries/Organization/QueryByNameOrCvrContent.cs
@@ -5,17 +5,16 @@ namespace Core.DomainServices.Queries.Organization
 {
     public class QueryByNameOrCvrContent : IDomainQuery<DomainModel.Organization.Organization>
     {
-        private readonly string _query;
-        private readonly string _queryLower;
+        private readonly string? _queryLower;
 
-        public QueryByNameOrCvrContent(string query)
+        public QueryByNameOrCvrContent(string? query)
         {
-            _query = query;
             _queryLower = query?.ToLower();
         }
 
         public IQueryable<DomainModel.Organization.Organization> Apply(IQueryable<DomainModel.Organization.Organization> source)
         {
+            if(_queryLower == null) return source;
             return source.Where(x => x.Cvr != null && x.Cvr.ToLower().Contains(_queryLower) || x.Name.ToLower().Contains(_queryLower));
         }
     }
