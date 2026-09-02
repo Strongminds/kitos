8f4899bb66442995087653ee9a380d69e6ddd489
Aleksander Naskret <aln@strongminds.dk>
2026-09-02 09:40:40 +0200

    remove duplicate using

--- Presentation.Web/Controllers/API/V2/Internal/Organizations/OrganizationSupplierInternalV2Controller.cs
+++ Presentation.Web/Controllers/API/V2/Internal/Organizations/OrganizationSupplierInternalV2Controller.cs
@@ -12,7 +12,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
-using Core.DomainModel.SupplierAssociatedFields;
 using Presentation.Web.Models.API.V2.Internal.Response.Organizations.Suppliers;
 
 namespace Presentation.Web.Controllers.API.V2.Internal.Organizations
