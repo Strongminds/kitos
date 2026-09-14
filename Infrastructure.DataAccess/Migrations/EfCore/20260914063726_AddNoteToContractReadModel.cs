c513d9d5d45118dade4994153048f4ce3f2cc290
Aleksander Naskret <aln@strongminds.dk>
2026-09-14 08:37:52 +0200

    add contract read model note migration

---
+++ Infrastructure.DataAccess/Migrations/EfCore/20260914063726_AddNoteToContractReadModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.DataAccess.Migrations.EfCore
+{
+    /// <inheritdoc />
+    public partial class AddNoteToContractReadModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                schema: "dbo",
+                table: "ItContractOverviewReadModels",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Note",
+                schema: "dbo",
+                table: "ItContractOverviewReadModels",
+                column: "Note");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Note",
+                schema: "dbo",
+                table: "ItContractOverviewReadModels");
+
+            migrationBuilder.DropColumn(
+                name: "Note",
+                schema: "dbo",
+                table: "ItContractOverviewReadModels");
+        }
+    }
+}
